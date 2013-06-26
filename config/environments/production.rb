ce1dbea3611a64dc7485091b2da27f6ddf3fee78
denishaskin <denis@constantorbit.com>
2013-06-26 08:45:29 -0400

    rails_apps_composer: set email accounts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,15 @@ Oneclick::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => 'example.com' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
