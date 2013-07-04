77940888ac2ba4c3cdf7956b6ccf10487b2b77db
denishaskin <denis@constantorbit.com>
2013-07-04 12:17:37 -0400

    Home page and top menus localized and branded.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,11 @@ class HomeController < ApplicationController
 
   def index
     @actions = [
-        {label: 'Plan a Trip', target: new_trips_path, icon: 'icon-bus-sign'},
-        {label: 'Identify Places', target: '#', icon: 'icon-map-marker'},
-        {label: 'Change My Settings', target: '#', icon: 'icon-cog'},
-        {label: 'Help & Support', target: '#', icon: 'icon-question-sign'},
+        {label: t(:plan_a_trip), target: new_trips_path, icon: 'icon-bus-sign'},
+        {label: t(:identify_places), target: '#', icon: 'icon-map-marker'},
+        {label: t(:change_my_settings), target: '#', icon: 'icon-cog'},
+        {label: t(:help_and_support), target: '#', icon: 'icon-question-sign'},
     ]
   end
-
+  
 end
