88fb67e52369418eb39770f19745236d59daf0fa
denishaskin <denis@constantorbit.com>
2013-07-24 15:22:30 -0400

    Fix typo in home_controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ include CsHelpers
     @actions = [
         {label: t(:plan_a_trip), target: new_trip_path, icon: ACTION_ICONS[:plan_a_trip]},
         {label: t(:identify_places), target: '#', icon: ACTION_ICONS[:identify_places]},
-        {label: t(:change_my_settings), target: '#', icon: ACTION_ICONS[:change_my_settings},
+        {label: t(:change_my_settings), target: '#', icon: ACTION_ICONS[:change_my_settings]},
         {label: t(:help_and_support), target: '#', icon: ACTION_ICONS[:help_and_support]},
     ]
   end
