f20e573441535d2c207951f438a808b17c664b79
denishaskin <denis@constantorbit.com>
2013-07-24 15:20:15 -0400

    Use ACTION_ICONS for consistent icons, fix up itinerary summary view to use model methods.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
+include CsHelpers
 
   def index
     @actions = [
-        {label: t(:plan_a_trip), target: new_trip_path, icon: 'icon-bus-sign'},
-        {label: t(:identify_places), target: '#', icon: 'icon-map-marker'},
-        {label: t(:change_my_settings), target: '#', icon: 'icon-cog'},
-        {label: t(:help_and_support), target: '#', icon: 'icon-question-sign'},
+        {label: t(:plan_a_trip), target: new_trip_path, icon: ACTION_ICONS[:plan_a_trip]},
+        {label: t(:identify_places), target: '#', icon: ACTION_ICONS[:identify_places]},
+        {label: t(:change_my_settings), target: '#', icon: ACTION_ICONS[:change_my_settings},
+        {label: t(:help_and_support), target: '#', icon: ACTION_ICONS[:help_and_support]},
     ]
   end
 
