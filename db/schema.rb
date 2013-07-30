98a9c9e81dcb80e111551be7136f66c9f79ab173
julianray <jray@camsys.com>
2013-07-30 13:11:36 -0400

    Ui cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130710175540) do
+ActiveRecord::Schema.define(:version => 20130726155019) do
 
   create_table "itineraries", :force => true do |t|
     t.integer  "duration"
