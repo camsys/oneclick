db1fe43bd1df5a072de198503c452bc8ce3d62fc
denishaskin <denis@constantorbit.com>
2013-08-06 11:40:14 -0400

    Make tests work again.

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
   factory :trip do
     trip_time "2:59 pm"
     trip_date (Date.today + 2).strftime('%m/%d/%Y')
+    arrive_depart 'depart_at'
     factory :trip_with_places do
       from_place FactoryGirl.create(:trip_place1)
       to_place FactoryGirl.create(:trip_place2)
