9c0ca53c5e9332ae4ceefec6ad534c802b59c0f0
julianray <jray@camsys.com>
2013-08-08 12:40:06 -0400

    Added subnav and controls for report filtering

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,11 @@
 class ReportsController < ApplicationController
   
+  TIME_FILTERS = [
+    {:id => 0, :value => "Last 30 days", :parse_text_start => "30 days ago", :parse_text_end => "yesterday", :is_day_duration => true},
+    {:id => 1, :value => "Last 7 days", :parse_text_start => "7 days ago", :parse_text_end => "yesterday", :is_day_duration => true},
+    {:id => 2, :value => "Last month", :parse_text_start => "last month", :parse_text_end => "last month", :is_day_duration => false}
+  ]
+  
   def index
     
     @reports = Report.all
@@ -10,12 +16,17 @@ class ReportsController < ApplicationController
   # from the view
   def show
     
-    # load this report and create the report instance 
-    @report = Report.find(params[:id])
-
+    # if a report_type parameter was passed use that otherwise use the id
+    @report = params[:report_id].blank? ? Report.find(params[:id]) : Report.find(params[:report_id])
+    
     if @report
-      @report_view = @report.view_name
+      # view params needed for the subnav filters
+      @report_id = @report.id
+      @time_filter_type = params[:time_filter_type]
       
+      # set up the report view
+      @report_view = @report.view_name
+      # get the class instance and generate the data
       report_instance = @report.class_name.constantize.new
       @data = report_instance.get_data(current_user, params)
       respond_to do |format|
--- app/reports/invalid_trips_report.rb
+++ app/reports/invalid_trips_report.rb
@@ -6,7 +6,8 @@ class InvalidTripsReport < AbstractReport
   
   def get_data(current_user, params)
     
-    return Trip.failed
+    duration = time_filter_as_duration(params[:time_filter_type])
+    return Trip.failed.where("created_at > ? AND created_at < ?", duration.first.at_beginning_of_day, duration.last.tomorrow.at_beginning_of_day)
 
   end
     
