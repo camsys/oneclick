e2eb115a90cef10669f248d96a4f80781cd90833
julianray <jray@camsys.com>
2013-08-14 12:52:58 -0400

    Added loader for POIs

--- script/data_loaders/poi_loader.rb
+++ script/data_loaders/poi_loader.rb
@@ -2,8 +2,15 @@
 
 #
 # Loads a POI file in GPX format into the database. Uses Nokogiri to parse
-# the file
+# the file. A typical record look like
 #
+# <wpt lat="32.916792500" lon="-85.401615000">
+#   <name>Hospital:Batson Memorial Sanitarium</name>
+#   <cmt>Hospital:Batson Memorial Sanitarium</cmt>
+#   <desc>Hospital:Batson Memorial Sanitarium</desc>
+# </wpt>
+#
+
 require File.dirname(__FILE__) + '/../../config/environment.rb' 
 require "rubygems"
 
@@ -11,15 +18,15 @@ require "rubygems"
 STATE = 'georgia'
 DATA_PATH = "../../db/poi_data/"
 POI_CLASSES = [
-  'Automotive', 
-  'Eating_Drinking', 
+  #'Automotive', 
+  #'Eating_Drinking', 
   'Government_and_Public_Services', 
   'Health_care', 
-  'Leisure', 
-  'Lodging', 
-  'Night_life_and_Business',
-  'Sports',
-  'Tourism'
+  #'Leisure', 
+  #'Lodging', 
+  #'Night_life_and_Business',
+  #'Sports',
+  #'Tourism'
   ]
   
 folder_path = DATA_PATH + STATE
@@ -38,6 +45,37 @@ POI_CLASSES.each do |poi_class|
   Rails.logger.info("  Version " + root["version"] + " Creator " + root["creator"])
   pois = root.xpath("wpt")  
   Rails.logger.info("  Found " + pois.count.to_s + " records.")
+  
+  pois.each do |poi|
+    lat = poi["lat"].to_d
+    lon = poi["lon"].to_d
+    text = poi.xpath("name").text
+    # Split on the separator as the poi names are sub-type:name eg Hospital:Batson Memorial Sanitarium
+    elems = text.split(":")
+    if elems.count > 1
+      poi_type_name = elems[0].strip
+      poi_name = elems[1].strip
+      
+      # See if we can find the poi type
+      poi_type = PoiType.find_by_name(poi_type_name)
+      if poi_type.nil?
+        poi_type = PoiType.new
+        poi_type.name = poi_type_name
+        poi_type.active = true
+        poi_type.save!
+      end
+      
+      # Create the poi
+      poi = Poi.new
+      poi.name = poi_name
+      poi.poi_type = poi_type
+      poi.lat = lat
+      poi.lon = lon
+      poi.save!
+    else
+      Rails.logger.info("    Skipping " + text)
+    end
+  end
   
   f.close
     
