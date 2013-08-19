692333f76ec727cdef76f3742045d4e58d2d5013
denishaskin <denis@constantorbit.com>
2013-08-19 15:24:10 -0400

    Fixing up localization, ride share handling geocode results without addresses.

--- lib/service_adapters/rideshare_adapter.rb
+++ lib/service_adapters/rideshare_adapter.rb
@@ -20,10 +20,11 @@ module ServiceAdapters
           query["#{p}.geocodeType"] = 'Address'
           query["#{p}.latLon.x"] = s2.lon
           query["#{p}.latLon.y"] = s2.lat
-        # query["#{p}.line2"] = to.address2
-        query["#{p}.postalCode"] = s1['postal_code']['short_name']
-        query["#{p}.state"] = s1['administrative_area_level_1']['short_name']
-        query["#{p}.street"] = s1['street_number']['short_name'] + ' ' + s1['route']['short_name']
+          # query["#{p}.line2"] = to.address2
+          query["#{p}.postalCode"] = s1['postal_code']['short_name']
+          query["#{p}.state"] = s1['administrative_area_level_1']['short_name']
+          query["#{p}.street"] = s1['street_number']['short_name'] + ' ' + s1['route']['short_name'] rescue s1['premise']['short_name'] + ' ' + s1['route']['short_name'] rescue
+            s1['route']['short_name'] rescue s1['premise']['short_name'] rescue ''
         # query['dest.suburb'] = 
       end
       query['search'] = 'search'
