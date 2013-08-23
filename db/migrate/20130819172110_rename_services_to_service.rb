43e39d28116060793ad688bb4367c11d391aeb3d
denishaskin <denis@constantorbit.com>
2013-08-23 18:08:17 -0400

    Remove renaming migrations; schema.rb already has the tables with the correct names.

--- db/migrate/20130819172110_rename_services_to_service.rb
+++
@@ -1,9 +0,0 @@
-class RenameServicesToService < ActiveRecord::Migration
-  def up
-    rename_table :services, :services
-  end
-
-  def down
-    rename_table :services, :services
-  end
-end
--- db/migrate/20130819172131_rename_traveler_accommodations_to_traveler_accommodation.rb
+++
@@ -1,9 +0,0 @@
-class RenameTravelerAccommodationsToTravelerAccommodation < ActiveRecord::Migration
-  def up
-    rename_table :traveler_accommodations, :traveler_accommodations
-  end
-
-  def down
-    rename_table :traveler_accommodations, :traveler_accommodations
-  end
-end
