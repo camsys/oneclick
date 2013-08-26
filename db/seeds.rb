24e6295e9dca6f9b3e5225bda375e5b89d86d611
julianray <jray@camsys.com>
2013-08-26 14:50:51 -0400

    [Fixes #55553782]

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,7 +41,7 @@ modes = [
 reports = [
   {name: 'Trips By Day Report', description: 'Displays a chart showing the number of trips generated each day.', view_name: 'generic_report', class_name: 'TripsPerDayReport', active: 1}, 
   {name: 'Failed Trips Report', description: 'Displays a report describing the trips that failed.', view_name: 'trips_report', class_name: 'InvalidTripsReport', active: 1}, 
-  {name: 'Rejected Report', description: 'Displays a report showing trips that were rejected by a user.', view_name: 'trips_report', class_name: 'RejectedTripsReport', active: 1} 
+  {name: 'Rejected Trips Report', description: 'Displays a report showing trips that were rejected by a user.', view_name: 'trips_report', class_name: 'RejectedTripsReport', active: 1} 
 ]
 relationship_statuses = [
   {id: 1, name: 'Requested'},
