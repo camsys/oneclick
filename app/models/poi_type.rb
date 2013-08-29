cda2c61676f6c3c5c7c8b36551f66124b3621ce7
julianray <jray@camsys.com>
2013-08-29 14:35:41 -0400

    Added default sort to poi types. [Fixes #56056228]

--- app/models/poi_type.rb
+++ app/models/poi_type.rb
@@ -2,6 +2,8 @@ class PoiType < ActiveRecord::Base
   
   attr_accessible :name, :active
   
+  default_scope where("poi_types.active = ?", true).order("poi_types.name")
+  
   # Associations
   has_many :pois
   
