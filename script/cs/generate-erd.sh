2976af418858f35a6bbbf946020cab920556d755
denishaskin <denis@constantorbit.com>
2013-09-13 11:15:29 -0400

    Add the script to generate filtered ERD.

---
+++ script/cs/generate-erd.sh
@@ -0,0 +1,24 @@
+# rake erd title="Eligibility models, One-Click" filename=erd-filtered-horizontal only="CoverageArea,FareStructure,Schedule,Service,ServiceTravelerAccommodationsMap,ServiceTravelerCharacteristicsMap,ServiceTripPurposeMap,ServiceType,TravelerAccommodation,TravelerCharacteristic,Provider,TripPurpose,UserAccommodationsProxy,UserCharacteristicsProxy,UserModePreference,UserProfile,UserTravelerAccommodationsMap,UserTravelerCharacteristicsMap,Trip,User"
+rake erd title="Eligibility models - One-Click" filename=erd-filtered-vertical orientation=vertical only="CoverageArea,FareStructure,Schedule,Service,ServiceTravelerAccommodationsMap,ServiceTravelerCharacteristicsMap,ServiceTripPurposeMap,ServiceType,TravelerAccommodation,TravelerCharacteristic,Provider,TripPurpose,UserAccommodationsProxy,UserCharacteristicsProxy,UserModePreference,UserProfile,UserTravelerAccommodationsMap,UserTravelerCharacteristicsMap,Trip,User"
+open erd-filtered-vertical.pdf
+# open erd-filtered-horizontal.pdf
+
+
+
+# Itinerary
+# Mode
+# Place
+# PlaceProxy
+# PlannedTrip
+# Poi
+# PoiType
+# Property
+# RelationshipStatus
+# Report
+# Role
+# TripPlace
+# TripProxy
+# TripStatus
+# UserRelationship
+# UserRole
+# ValueRelationship
