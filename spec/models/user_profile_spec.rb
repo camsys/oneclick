2f02f046c3727bc331e764a39af9f829273fed6f
denishaskin <denis@constantorbit.com>
2013-09-17 15:54:40 -0400

    Fix the other tests to use size, not count.

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -6,26 +6,26 @@ describe UserProfile do
     user_profile = FactoryGirl.create(:user_profile)
     eh = EligibilityHelpers.new
     all_services = Service.all
-    all_services.count.should eq 5
+    expect(all_services.size).to eq 5
     services = eh.get_eligible_services_for_traveler(user_profile)
-    services.count.should eq 2
+    expect(services.size).to eq 2
     planned_trip = FactoryGirl.create(:trip_with_places)
     purpose = TripPurpose.find_by_name('Medical')
     planned_trip.trip.trip_purpose = purpose
     services = eh.get_eligible_services_for_trip(planned_trip, services)
-    services.count.should eq 2
+    expect(    services.size).to eq 2
   end
 
   it "has eligible services for traveler but not trip" do
     user_profile = FactoryGirl.create(:user_profile)
     eh = EligibilityHelpers.new
     services = eh.get_eligible_services_for_traveler(user_profile)
-    services.count.should eq 2
+    expect(    services.size).to eq 2
     planned_trip = FactoryGirl.create(:trip_with_places2)
     purpose = TripPurpose.find_by_name('Medical')
     planned_trip.trip.trip_purpose = purpose
     services = eh.get_eligible_services_for_trip(planned_trip, services)
-    services.count.should eq 0
+    expect(    services.size).to eq 0
   end
 
   it "is eligible for all five seeded services" do
@@ -57,6 +57,7 @@ describe UserProfile do
     purpose = TripPurpose.find_by_name('Medical')
     planned_trip.trip.trip_purpose = purpose
     services2 = eh.get_eligible_services_for_trip(planned_trip, acc_and_eligible_services)
+    puts services2.size
     expect(services2.size).to eq 5
   end
 
@@ -72,12 +73,12 @@ describe UserProfile do
       end
     end
     services = eh.get_eligible_services_for_traveler(user_profile)
-    services.count.should eq 5
+    expect(    services.size).to eq 5
     planned_trip = FactoryGirl.create(:trip_with_places)
     purpose = TripPurpose.find_by_name('Personal')
     planned_trip.trip.trip_purpose = purpose
     services = eh.get_eligible_services_for_trip(planned_trip, services)
-    services.count.should eq 0
+    expect(    services.size).to eq 0
   end
 
 end
