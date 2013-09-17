9a6a6d4e337253279d40998a583a877f8276bc03
denishaskin <denis@constantorbit.com>
2013-09-17 11:45:05 -0400

    Oops, do the coveralls setup correctly for a rails app.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'coveralls'
+Coveralls.wear!('rails')
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -7,8 +10,6 @@ require 'rspec/autorun'
 require 'capybara/rspec'
 require 'awesome_print'
 require 'factory_girl_rails'
-require 'coveralls'
-Coveralls.wear!
 
 I18n.locale = :en
 
