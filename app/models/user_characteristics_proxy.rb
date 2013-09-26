84fa094e4390247744c6760522c13c5f9fe82812
julianray <jray@camsys.com>
2013-09-26 12:02:59 -0400

    Modified accomodations and eligibility form to be more responsive and have standard layout. Changed date picker to use siple form date picker. Added date localizations for en and es

--- app/models/user_characteristics_proxy.rb
+++ app/models/user_characteristics_proxy.rb
@@ -1,11 +1,11 @@
 class UserCharacteristicsProxy < Proxy
 
-  attr_accessor :user, :dob_month, :dob_day, :dob_year
+  attr_accessor :user, :dob_month, :dob_day, :dob_year, :date_of_birth
   validate :validate_dob
 
   def initialize(user = nil)
     self.user = user
-
+    date_of_birth = Date.today
     super
   end
 
