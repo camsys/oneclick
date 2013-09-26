13341997971467db40185fb464917b3c866eb22d
julianray <jray@camsys.com>
2013-09-26 15:40:11 -0400

    Fixed validation logic on characteristics proxy class

--- app/controllers/user_characteristics_proxies_controller.rb
+++ app/controllers/user_characteristics_proxies_controller.rb
@@ -3,8 +3,9 @@ class UserCharacteristicsProxiesController < ApplicationController
   def update
 
     @user_characteristics_proxy = UserCharacteristicsProxy.new(User.find(params[:user_id]))
-    @user_characteristics_proxy.update_maps(params[:user_characteristics_proxy])
-    if @user_characteristics_proxy.validate_dob
+    
+    if @user_characteristics_proxy.valid?
+      @user_characteristics_proxy.update_maps(params[:user_characteristics_proxy])
       @user_characteristics_proxy.update_dob
       flash[:notice] = "Traveler characteristics successfully updated."
     end
