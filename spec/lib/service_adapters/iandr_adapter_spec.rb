6421d2626399bda8dace2aaa3313143fa5f45d0e
denishaskin <denis@constantorbit.com>
2013-11-06 15:23:07 -0500

    Yeah, running tests locally before committing is always a good idea.

--- spec/lib/service_adapters/iandr_adapter_spec.rb
+++ spec/lib/service_adapters/iandr_adapter_spec.rb
@@ -6,7 +6,8 @@ describe ServiceAdapters::IandrAdapter do
     p = Provider.new
     p.services << Service.new
     i = ServiceAdapters::IandrAdapter.new([p])
-    puts i.to_xml(indent: 2)
-    i.to_xml.should eq 'foo'
+    # TODO this doesn't current have a real test, I just use it to generate sample XML
+    # puts i.to_xml(indent: 2)
+    # i.to_xml.should eq 'foo'
   end
 end
