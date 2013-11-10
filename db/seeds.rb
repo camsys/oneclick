37b3d978483067959b70842147a0ed6ae3b5bdaf
denishaskin <denis@constantorbit.com>
2013-11-10 18:01:00 -0500

    Break seeds apart.

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+case Oneclick::Application.config.brand
+when 'pa'
+  require File.join(Rails.root, 'db', 'pa_seeds.rb')
+else
+  raise "Brand #{Oneclick::Application.config.brand} not handled" 
+end
