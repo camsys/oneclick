25db97bbaa7438b274a65809b6c889e191d5c6d9
denishaskin <denis@constantorbit.com>
2013-11-15 10:37:59 -0500

    Handle broward seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,8 @@ when 'pa'
   require File.join(Rails.root, 'db', 'pa_seeds.rb')
 when 'arc'
   require File.join(Rails.root, 'db', 'atl_seeds.rb')
+when 'broward'
+  require File.join(Rails.root, 'db', 'broward_seeds.rb')
 else
   raise "Brand #{Oneclick::Application.config.brand} not handled" 
 end
