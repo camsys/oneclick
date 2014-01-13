00f50c310639109e55ef05c6119331d3cf0b6d9b
dedwards8 <dedwards8@gmail.com>
2014-01-13 16:43:06 -0500

    [Fixes#52124389] A viewer is availeble for each service.  An admin can view accommodations, eligibility, schedule, fare, and coverage area information for every service.  This information is available from the Admin/View Services.

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -3,6 +3,12 @@ class ServicesController < ApplicationController
   include ApplicationHelper
 
   def show
+    @services = Service.all(:order => "name")
+
+    if params['service']
+      params[:id] = params['service']['id']
+    end
+
     @service = Service.find(params[:id])
     @eh = EligibilityHelpers.new
     respond_to do |format|
--- lib/cs_helpers.rb
+++ lib/cs_helpers.rb
@@ -14,7 +14,8 @@ module CsHelpers
     :create_traveler =>'icon-user',
     :agents_agencies => 'icon-umbrella',
     :reports => 'icon-bar-chart',
-    :trips => 'icon-tags'
+    :trips => 'icon-tags',
+    :services => 'icon-bus-sign'
   }
   # Session key for storing the traveler id
   TRAVELER_USER_SESSION_KEY = 'traveler'
