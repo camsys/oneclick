9aee6b23a077242d72bd6787169e7ed7246c4d70
Quinn Shanahan <q.shanahan@gmail.com>
2014-01-27 16:10:32 -0600

    update to use kiosk namespace

--- app/assets/javascripts/users/users.js
+++ app/assets/javascripts/users/users.js
@@ -3,12 +3,12 @@ var loginView = {
 
 loginView.init = function () {
 
-  $('input#user_email').click(function(){
+  $('input#kiosk_user_email').click(function(){
        if (window.cocoa)
         window.cocoa.openKeyboard();
   });
 
-  $('input#user_password').click(function(){
+  $('input#kiosk_user_password').click(function(){
        if (window.cocoa)
         window.cocoa.openKeyboard();
   });
