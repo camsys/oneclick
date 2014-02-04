50a54c932ccf9db86fbf5cb4b1abd2972da98d95
denishaskin <denis@constantorbit.com>
2014-02-04 03:11:14 -0500

    We need last migration.

--- db/migrate/20140202155209_provider_org_mods.rb
+++ db/migrate/20140202155209_provider_org_mods.rb
@@ -1,6 +1,6 @@
 class ProviderOrgMods < ActiveRecord::Migration
   def change
-    rename_column :organizations, :org_type, :type
+    # rename_column :organizations, :org_type, :type
     add_column :providers, :provider_org_id, :integer
   end
 
