c5bf8f8f1812552465c21d3a00b61d4577ae970e
Quinn Shanahan <q.shanahan@gmail.com>
2014-02-11 10:53:53 -0600

    No longer using this. Not necessary.

--- app/models/trip/validation_wrapper/pickup_time.rb
+++ app/models/trip/validation_wrapper/pickup_time.rb
@@ -2,7 +2,6 @@ class Trip::ValidationWrapper::PickupTime < Trip::ValidationWrapper::Base
   include Trip::PickupTime
 
   def as_json *args
-    @trip_datetime = trip_datetime
     @return_trip_time = (trip_datetime + TripsSupport::DEFAULT_RETURN_TRIP_DELAY_MINS.minutes).strftime(TripsSupport::TRIP_TIME_FORMAT_STRING)
     super
   end
