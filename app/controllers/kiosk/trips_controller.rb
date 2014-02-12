82ca0fe1616578ccfba2ac4b66b64c4a75f238bd
Quinn Shanahan <q.shanahan@gmail.com>
2014-02-12 15:31:40 -0600

    Fix for final page

--- app/controllers/kiosk/trips_controller.rb
+++ app/controllers/kiosk/trips_controller.rb
@@ -2,11 +2,18 @@ module Kiosk
   class TripsController < ::TripsController
     include Behavior
 
-    def start
-      get_traveler
-      get_trip
+    # def start
+    #   get_traveler
+    #   get_trip
 
-      @trip_proxy = create_trip_proxy(@trip)
+    #   @trip_proxy = create_trip_proxy(@trip)
+    # end
+  protected
+
+    def back_url
+      if params[:action] == 'show'
+        '/'
+      end
     end
   end
 end
