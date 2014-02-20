d47421dd44a7ae1d82cdb688bf0803a063710e7f
Quinn Shanahan <q.shanahan@gmail.com>
2014-02-20 17:24:47 -0600

    remove poi_id from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,6 @@ end
 
 KioskLocation.create!([{
   name: 'machine1',
-  poi_id: 8768,
   address_type: 6,
   addr: '828 Mitchell Street Southwest, Atlanta, GA 30314',
   lat: 33.7532,
@@ -19,7 +18,6 @@ KioskLocation.create!([{
 },
 {
   name: 'machine2',
-  poi_id: 8813,
   address_type: 6,
   addr: '2891 Lakewood Avenue Southwest, Atlanta, GA 30315',
   lat: 33.6973,
