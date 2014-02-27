52525f952405b7c20fab06deb9b58b1073e3010e
Quinn Shanahan <q.shanahan@gmail.com>
2014-02-27 16:08:16 -0600

    Do not enable postgis

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20140220224403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "postgis"
 
   create_table "accommodations", force: true do |t|
     t.string  "name",                  limit: 64,                 null: false
