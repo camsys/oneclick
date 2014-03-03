b107f3688bb3f3e45fbbf5acf69d32f79dcad18e
Quinn Shanahan <q.shanahan@gmail.com>
2014-03-02 19:36:35 -0600

    Show different center and zoom for PA

--- app/helpers/leaflet_helper.rb
+++ app/helpers/leaflet_helper.rb
@@ -1,5 +1,5 @@
 module LeafletHelper
-  
+
   # Defaults
   MAPID = "map"
   MINZOOM = 1
@@ -7,19 +7,19 @@ module LeafletHelper
   TILE_PROVIDER = 'CLOUDMADE'
   TILE_STYLE_ID = 997
   MAP_BOUNDS = Rails.application.config.map_bounds
-  
+
   def LeafletMap(options)
     options_with_indifferent_access = options.with_indifferent_access
-    
+
     js_dependencies = Array.new
     #js_dependencies << 'http://cdn.leafletjs.com/leaflet-0.4/leaflet.js'
     #js_dependencies << 'leafletmap.js_x'
     #js_dependencies << 'leafletmap_icons.js_x'
 
-    render :partial => '/leaflet/leaflet', :locals  => { :options => options_with_indifferent_access, :js_dependencies => js_dependencies }
-    
+    render :partial => '/leaflet/leaflet', :locals => { :options => options_with_indifferent_access, :js_dependencies => js_dependencies }
   end
-  # Generates the Leaflet JS code to create the map from the options hash 
+
+  # Generates the Leaflet JS code to create the map from the options hash
   # passed in via the LeafletMap helper method
   def generate_map(options)
     js = []
@@ -31,14 +31,15 @@ module LeafletHelper
     max_zoom = options[:max_zoom] ? options[:max_zoom] : MAXZOOM
     tile_provider = options[:tile_provider] ? options[:tile_provider] : TILE_PROVIDER
     tile_style_id = options[:tile_style_id] ? options[:tile_style_id] : TILE_STYLE_ID
+
     mapopts = {
-      :min_zoom => min_zoom, 
-      :max_zoom => max_zoom, 
+      :min_zoom => min_zoom,
+      :max_zoom => max_zoom,
       :tile_provider => tile_provider,
       :tile_style_id => tile_style_id
     }.to_json
-    js << "init('#{mapid}', #{mapopts});"
 
+    js << "init('#{mapid}', #{mapopts});"
     # add any markers
     js << "addMarkers(#{options[:markers]});" unless options[:markers].nil?
     # add any circles
@@ -49,6 +50,7 @@ module LeafletHelper
     js << "setMapBounds(#{MAP_BOUNDS[0][0]},#{MAP_BOUNDS[0][1]},#{MAP_BOUNDS[1][0]},#{MAP_BOUNDS[1][1]});"
     js << "cacheMapBounds(#{MAP_BOUNDS[0][0]},#{MAP_BOUNDS[0][1]},#{MAP_BOUNDS[1][0]},#{MAP_BOUNDS[1][1]});"
     js << "showMap();"
+    js << "LMmap.setZoom(#{options[:zoom]});" if options[:zoom].present?
     js * ("\n")
   end
 end
--- config/initializers/_oneclick.rb
+++ config/initializers/_oneclick.rb
@@ -13,7 +13,7 @@ when 'arc'
   Oneclick::Application.config.ui_logo = 'arc/arc-logo.png'
   Oneclick::Application.config.geocoder_components = 'administrative_area:GA|country:US'
   Oneclick::Application.config.map_bounds = [[33.457797,-84.754028], [34.090199,-83.921814]]
-  Oneclick::Application.config.geocoder_bounds = [[33.737147,-84.406634], [33.764125,-84.370361]]  
+  Oneclick::Application.config.geocoder_bounds = [[33.737147,-84.406634], [33.764125,-84.370361]]
   Oneclick::Application.config.open_trip_planner = "http://otp-arc.camsys-apps.com"
   Oneclick::Application.config.taxi_fare_finder_api_key = "SIefr5akieS5"
   Oneclick::Application.config.taxi_fare_finder_api_city = "Atlanta"
@@ -30,7 +30,7 @@ when 'arc'
   honeybadger_api_key = 'ba642a71'
   Oneclick::Application.config.poi_file = 'db/arc_poi_data/CommFacil_20131015.txt'
 
-when 'broward'  
+when 'broward'
   Oneclick::Application.config.ui_logo = 'broward/Broward_211_Get_Connected_get_answers.jpg'
   Oneclick::Application.config.geocoder_components = 'administrative_area:FL|country:US'
   Oneclick::Application.config.map_bounds = [[26.427309, -80.347081], [25.602294, -80.061728]]
@@ -52,8 +52,9 @@ when 'broward'
 when 'pa'
   Oneclick::Application.config.ui_logo = 'pa/penndotLogo.jpg'
   Oneclick::Application.config.geocoder_components = 'administrative_area:PA|country:US'
-  Oneclick::Application.config.map_bounds = [[41.970622, -80.461542], [39.734653, -75.007294]]
-  Oneclick::Application.config.geocoder_bounds = [[41.970622, -80.461542], [39.734653, -75.007294]]
+  Oneclick::Application.config.map_bounds      = [[40.0262999543423,  -76.56372070312499], [39.87970800405549, -76.90189361572266]]
+  Oneclick::Application.config.geocoder_bounds = [[40.0262999543423,  -76.56372070312499], [39.87970800405549, -76.90189361572266]]
+  Oneclick::Application.config.default_zoom = 12
   Oneclick::Application.config.open_trip_planner = "http://otp-pa.camsys-apps.com:8080"
   Oneclick::Application.config.taxi_fare_finder_api_key = "SIefr5akieS5"
   Oneclick::Application.config.taxi_fare_finder_api_city = "Harrisburg-PA"
@@ -87,9 +88,9 @@ end
 # General UI configuration settings
 Oneclick::Application.config.ui_typeahead_delay = 300       # milliseconds delay between keystrokes before a query is sent to the server to retrieve a typeahead list
 Oneclick::Application.config.ui_typeahead_min_chars = 4     # minimum number of characters to initiate a query
-Oneclick::Application.config.ui_typeahead_list_length = 10  # max number of items displayed in the typeahead list  
-Oneclick::Application.config.ui_search_poi_items = 10       # max number of matching POIs to return in a search 
-Oneclick::Application.config.ui_min_geocode_chars = 5       # Minimum number of characters (not including whitespace) before sending to the geocoder 
+Oneclick::Application.config.ui_typeahead_list_length = 10  # max number of items displayed in the typeahead list
+Oneclick::Application.config.ui_search_poi_items = 10       # max number of matching POIs to return in a search
+Oneclick::Application.config.ui_min_geocode_chars = 5       # Minimum number of characters (not including whitespace) before sending to the geocoder
 
 Oneclick::Application.config.address_cache_expire_seconds = 3600 # seconds to keep addresses returned from the geocoder in the cache
 Oneclick::Application.config.return_trip_delay_mins = 120   # minutes needed at last trip place before scheduling the return trip
