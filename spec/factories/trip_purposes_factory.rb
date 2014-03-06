e0ec82b834edd485510bdc0ec02bdc6bb8c2b88c
denishaskin <denis@constantorbit.com>
2014-03-06 16:35:27 -0500

    Fix CI tests.

--- spec/factories/trip_purposes_factory.rb
+++ spec/factories/trip_purposes_factory.rb
@@ -1,7 +1,14 @@
 FactoryGirl.define do
 
-  factory :trip_purpose do
-    name 'Trip purpose #1'
-  end
+  # NOTE Do not create TripPurposes with factory; they are seed data and
+  # so will be put there by db:seeds
+  
+  # factory :trip_purpose do
+  #   sequence(:name) {|n| "Trip purpose #{n}"}
+  #   sequence(:code) do |n|
+  #     puts "In factory sequence, n is #{n}"
+  #     "PURPOSE#{n}"
+  #   end
+  # end
 
 end
