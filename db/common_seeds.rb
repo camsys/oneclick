8afa03531ef901a96f14c3da976b404a82401993
Aaron Magil <amagil@camsys.com>
2014-03-13 14:55:55 -0400

    Fixes a bunch of emails to use CMS content.  Moves CMS content keys to common samples, out of seeds

--- db/common_seeds.rb
+++ db/common_seeds.rb
@@ -58,37 +58,3 @@ u.save!
   structured_hash = structure_records_from_flat_hash record
   build_internationalized_records structured_hash
 end
-
-#Set up the common CMS tags with non-branded defaults
-text = <<EOT
-<h2 style="text-align: justify;">1-Click helps you find options to get from here to there, using public transit,
- door-to-door services, and specialized transportation.  Give it a try, and
- <a href="mailto://OneClick@camsys.com">tell us</a> what you think.</h2>
-EOT
-Translation.create!(:key =>'home-top_html', :locale => :en, :value => text)
-Translation.create!(:key =>'home-top-logged-in_html', :locale => :en, :value => text)
-text = <<EOT
-1-Click was funded by the
- <a href="http://www.fta.dot.gov/grants/13094_13528.html" target=_blank>Veterans Transportation
- Community Living Initiative</a>.
-EOT
-Translation.create!(:key =>'home-bottom-left-logged-in_html', :locale => :en, :value => text)
-Translation.create!(:key =>'home-bottom-left_html', :locale => :en, :value => text)
-text = <<EOT
-<span style="float: right;">1-Click is brought to you by 
-<a href="http://www.camsys.com/" target=_blank>Cambridge Systematics, Inc.</a>.</span>
-EOT
-Translation.create!(:key =>'home-bottom-right-logged-in_html', :locale => :en, :value => text)
-Translation.create!(:key =>'home-bottom-right_html', :locale => :en, :value => text)
-text = <<EOT
-Tell us about your trip.  The more information you give us, the more options we can find!
-EOT
-Translation.create(:key =>'plan-a-trip_html', :locale => :en, :value => text)
-Translation.create(:key => 'home-bottom-center_html', locale: :en, complete: true)
-Translation.create(:key => 'home-bottom-center-logged-in_html', locale: :en, complete: true)
-
-text = "In order to personalize the trip results further, would you please tell us about the programs you currently participate in?"
-Translation.create(:key => 'gather-program-info_html', locale: :en, complete: true, value: text)
-
-text= "Registering for ARC One-Click allows you to save your eligibility and needs information for planning future trips, as well as the ability to save and reuse trips, set up a travel buddy, and more."
-Translation.create(:key => 'registration-reasoning', locale: :en, complete: true, value: text)
