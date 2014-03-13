e9b339648c23409499cf46bea4cc41cd5102949f
Quinn Shanahan <q.shanahan@gmail.com>
2014-03-13 05:27:29 -0500

    Basic back button implementation.

--- app/controllers/kiosk/trips_controller.rb
+++ app/controllers/kiosk/trips_controller.rb
@@ -2,6 +2,16 @@ module Kiosk
   class TripsController < ::TripsController
     include Behavior
 
+    def show
+      if params[:back]
+        session[:current_trip_id] = @trip.id
+        redirect_to new_user_characteristic_path_for_ui_mode(@traveler, inline: 1)
+        return
+      end
+
+      super
+    end
+
     def itinerary_print
       @itinerary = Itinerary.find(params[:id])
       @legs = @itinerary.get_legs
@@ -13,7 +23,7 @@ module Kiosk
 
     def back_url
       if params[:action] == 'show'
-        '/'
+        url_for(back: true)
       end
     end
   end
