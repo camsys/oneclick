9ecc8e15612ace1b6479d97e7a5db28523281524
Aaron Magil <amagil@camsys.com>
2014-03-17 13:43:57 -0400

    Removes the link to define undefined keys.

--- lib/locale_helpers.rb
+++ lib/locale_helpers.rb
@@ -12,7 +12,7 @@ module LocaleHelpers
 def cms_snippet_content(key)
     { :locale => ((I18n.locale == I18n.default_locale) ? nil : I18n.locale) }
     out = translate(key)
-     return (out.to_str.include? 'class="translation_missing"') ? (link_to "Define #{key}", new_translation_path(:key => key, :locale => I18n.locale)).html_safe : out   # Display a link to define the translation if it doesn't exist.
+     return (out.to_str.include? 'class="translation_missing"') ? "" : out   # (link_to "Define #{key}", new_translation_path(:key => key, :locale => I18n.locale)).html_safe
 end
 
 end
