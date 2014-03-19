5bd7ad510d6c9321ebad8100ea5117c355a1f809
dedwards8 <dedwards8@gmail.com>
2014-03-19 14:28:24 -0400

    [#65675824] Improved error reporting from ESP Loader.  Also do initial checks to ensure that all the columns needed for a full esp import are present before importing any data.

--- app/controllers/esp_reader_controller.rb
+++ app/controllers/esp_reader_controller.rb
@@ -10,8 +10,27 @@ class EspReaderController < ApplicationController
   def update
 
     esp = EspReader.new
-    esp.unpack(params[:esp_upload][:zip].tempfile.path)
-    redirect_to confirm_esp_reader_index_path
+
+    if params[:esp_upload].nil?
+      flash[:error] = "No file selected."
+      @path = upload_esp_reader_index_path
+
+    else
+      result, message = esp.unpack(params[:esp_upload][:zip].tempfile.path)
+      if result
+        @path = services_path
+        flash[:notice] = "ESP services updated successfully."
+      else
+        @path = upload_esp_reader_index_path
+        flash[:error] = message
+      end
+
+    end
+
+    respond_to do |format|
+      format.html { redirect_to @path }
+      #format.js { render "esp_reader/uploadd" }
+    end
 
   end
 
