699366bf1df1ec6da4d419256108e33be39894b3
Aaron Magil <amagil@camsys.com>
2014-03-24 15:19:41 -0400

    Typo in validation in Translation

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,6 +1,6 @@
 class Translation < ActiveRecord::Base
 
-    validates_uniqueness_of :key, scope: :locale, message: "Keys should occur once per language"
+    validates_uniqueness_of :key, scope: :locale, message: "should occur once per language"
     
     validates :key, presence: true
 
