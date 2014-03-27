3a670c0489d22849be0b9b996bcc6037bb2c789f
Aaron Magil <amagil@camsys.com>
2014-03-27 10:33:52 -0400

    Reduces validation on translations, making it easier to seed and add sample data

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -3,8 +3,7 @@ class Translation < ActiveRecord::Base
     validates_uniqueness_of :key, scope: :locale, message: "should occur once per language"
     
     validates :key, presence: true
-
-    validates :locale, :value, presence: true
+    validates :locale, presence: true
 
     def self.available_locales
        Translation.uniq.pluck(:locale)
