9dfd630ff638ccbbe059e24b15a140f05d7c29b9
Aaron Magil <amagil@camsys.com>
2014-04-08 14:29:47 -0400

    updating trip_spec to use factory data

--- spec/factories/trip_purposes_factory.rb
+++ spec/factories/trip_purposes_factory.rb
@@ -1,14 +1,11 @@
 FactoryGirl.define do
-
-  # NOTE Do not create TripPurposes with factory; they are seed data and
-  # so will be put there by db:seeds
   
-  # factory :trip_purpose do
-  #   sequence(:name) {|n| "Trip purpose #{n}"}
-  #   sequence(:code) do |n|
-  #     puts "In factory sequence, n is #{n}"
-  #     "PURPOSE#{n}"
-  #   end
-  # end
+  factory :trip_purpose do
+    sequence(:name) {|n| "Trip purpose #{n}"}
+    sequence(:code) do |n|
+      puts "In factory sequence, n is #{n}"
+      "PURPOSE#{n}"
+    end
+  end
 
 end
