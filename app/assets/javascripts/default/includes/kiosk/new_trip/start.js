8702848ccdc26a4c7243392e0b029ac95c543a65
denishaskin <denis@constantorbit.com>
2014-04-14 22:01:46 -0400

    Corrections to merge to make the js/css split work.

--- app/assets/javascripts/default/includes/kiosk/new_trip/start.js
+++
@@ -1,6 +0,0 @@
-jQuery(function ($) {
-  if (!$('.js-trip-wizard-form').hasClass('js-start-wizard-step')) return;
-
-  NewTrip.start();
-  window.location = String(window.location).replace('start', 'from');
-});
