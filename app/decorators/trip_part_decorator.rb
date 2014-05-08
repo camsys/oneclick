8270f45ee5d7707cff3751aff16089c0a679b3d3
denishaskin <denis@constantorbit.com>
2014-05-08 14:30:20 -0400

    Move description to new TripPartDecorator

---
+++ app/decorators/trip_part_decorator.rb
@@ -0,0 +1,34 @@
+class TripPartDecorator < Draper::Decorator
+  delegate_all
+
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+
+  def description
+    # "Outbound - 40 Courtland Street NE Atlanta, GA to Atlanta VA Medical Center"
+    trip = object.trip
+    "%s - %s" % [
+      direction, description_without_direction
+    ]
+  end
+
+  def description_without_direction
+    # "Outbound - 40 Courtland Street NE Atlanta, GA to Atlanta VA Medical Center"
+    trip = object.trip
+    ("%s %s %s" % [
+      object.from_trip_place.name2, I18n.t(:to).downcase, 
+      object.to_trip_place.name2
+    ]).strip
+  end
+
+  def direction
+    object.is_return_trip? ? I18n.t(:return) : I18n.t(:outbound)
+  end
+
+end
--- app/serializers/trip_part_serializer.rb
+++ app/serializers/trip_part_serializer.rb
@@ -5,33 +5,12 @@ class TripPartSerializer < ActiveModel::Serializer
   attribute :is_depart, key: :is_depart_at
   has_many :itineraries
 
-  def description
-    # "Outbound - 40 Courtland Street NE Atlanta, GA to Atlanta VA Medical Center"
-    # Return
-    # out = trip_parts.first
-    trip = object.trip
-    "%s - %s %s %s" % [
-      direction, object.from_trip_place.name2, I18n.t(:to).downcase, 
-      object.to_trip_place.name2
-    ]
-    # s = [round_trip(trip),
-    # I18n.t(:from).downcase,
-    # trip.from_place.name,
-    # I18n.t(:to).downcase,
-    # trip.to_place.name,
-    # depart_arrive.downcase,
-    # format_date(object.trip_time),
-    # I18n.t(:at),
-    # format_time(object.trip_time),
-    # ].join ' '
+  def initialize(object, options={})
+    super(TripPartDecorator.new(object), options)
   end
 
   def round_trip trip
     trip.is_return_trip ? I18n.t(:round_trip) : I18n.t(:one_way)
-  end
-
-  def direction
-    object.is_return_trip? ? I18n.t(:return) : I18n.t(:outbound)
   end
 
   def start_time
