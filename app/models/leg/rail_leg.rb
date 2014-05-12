130b8742648264b883e66484a769fd369a936215
denishaskin <denis@constantorbit.com>
2014-05-12 14:54:43 -0400

    Improve informaiton in tooltips (at least for transit trips for now.) [#71095976]

--- app/models/leg/rail_leg.rb
+++ app/models/leg/rail_leg.rb
@@ -1,26 +1,9 @@
 module Leg
-  class RailLeg < TripLeg
-
-    attr_accessor :route
-    attr_accessor :route_short_name
-    attr_accessor :route_long_name
-    attr_accessor :route_id
-    attr_accessor :head_sign
-    attr_accessor :agency_name
-    attr_accessor :agency_id
+  class RailLeg < TransitLeg
 
     def initialize(attrs = {})
       super(attrs)
-      attrs.each do |k, v|
-        self.send "#{k}=", v
-      end
-
       self.mode = RAIL
-
-    end
-
-    def short_description
-      [I18n.t(mode.downcase.to_sym), head_sign, I18n.t(:to), end_place.name].join(' ')
     end
 
   end
