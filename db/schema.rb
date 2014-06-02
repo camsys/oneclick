7f882e750c7a10e1bff6af53a86537447442d41a
denishaskin <denis@constantorbit.com>
2014-06-02 13:46:00 -0400

    Get schema current, don't db:migrate on travis.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140523191229) do
+ActiveRecord::Schema.define(version: 20140529160145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
