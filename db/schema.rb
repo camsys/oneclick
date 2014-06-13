a9dca275f4a143b4a756f6b20bc78cff12ad1454
Xudong Liu <xudongliu@camsys.com>
2014-06-13 16:14:07 -0400

    schema update after migration

--- db/schema.rb
+++ db/schema.rb
@@ -212,6 +212,19 @@ ActiveRecord::Schema.define(version: 20140611204450) do
     t.string  "internal_contact_email", limit: 128
   end
 
+  create_table "rates", force: true do |t|
+    t.integer  "rater_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.integer  "stars",         null: false
+    t.string   "dimension"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "rates", ["rateable_id", "rateable_type"], :name => "index_rates_on_rateable_id_and_rateable_type"
+  add_index "rates", ["rater_id"], :name => "index_rates_on_rater_id"
+
   create_table "ratings", force: true do |t|
     t.integer  "user_id"
     t.integer  "rateable_id"
