d3ad565ad89cc8adac568c836c2e7d4b90457a56
Aaron Magil <amagil@camsys.com>
2014-06-16 14:25:37 -0400

    [Fixes#72468332] Removes server-side filtering for trips, leaves us with just client-side datatables to handle sorting/filtering

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -9,32 +9,33 @@ class TripsController < PlaceSearchingController
     :show_printer_friendly, :example, :plan, :populate]
 
   def index
-    # Filtering logic. See ApplicationHelper.trip_filters
-    if params[:time_filter_type]
-      @time_filter_type = params[:time_filter_type]
-    else
-      @time_filter_type = session[TIME_FILTER_TYPE_SESSION_KEY]
-    end
-    # if it is still not set use the default
-    if @time_filter_type.nil?
-      # default is to use the first time period filter in the TimeFilterHelper class
-      @time_filter_type = "100"
-    end
-    # store it in the session
-    session[TIME_FILTER_TYPE_SESSION_KEY] = @time_filter_type
+    # # Filtering logic. See ApplicationHelper.trip_filters
+    # if params[:time_filter_type]
+    #   @time_filter_type = params[:time_filter_type]
+    # else
+    #   @time_filter_type = session[TIME_FILTER_TYPE_SESSION_KEY]
+    # end
+    # # if it is still not set use the default
+    # if @time_filter_type.nil?
+    #   # default is to use the first time period filter in the TimeFilterHelper class
+    #   @time_filter_type = "100"
+    # end
+    # # store it in the session
+    # session[TIME_FILTER_TYPE_SESSION_KEY] = @time_filter_type
 
-    # If the filter is at least 100 is must be a time filter, otherwise it will be a TripPurpose
-    if @time_filter_type.to_i >= 100
-      actual_filter = @time_filter_type.to_i - 100
-      # get the duration for this time filter
-      duration = TimeFilterHelper.time_filter_as_duration(actual_filter)
-      @trips = @traveler.trips.scheduled_between(duration.first, duration.last)
-    else
-      # the filter is a trip purpose
-      # Okay to leave as UTC since we're just sorting by it?
-      @trips = @traveler.trips.where('trip_purpose_id = ?', @time_filter_type).sort_by {|x| x.trip_datetime }.reverse
-    end
-
+    # # If the filter is at least 100 is must be a time filter, otherwise it will be a TripPurpose
+    # if @time_filter_type.to_i >= 100
+    #   actual_filter = @time_filter_type.to_i - 100
+    #   # get the duration for this time filter
+    #   duration = TimeFilterHelper.time_filter_as_duration(actual_filter)
+    #   @trips = @traveler.trips.scheduled_between(duration.first, duration.last) # TODO Couple this to params better.  Runs through @traveler, which isn't quite right.
+    #   puts duration
+    # else
+    #   # the filter is a trip purpose
+    #   # Okay to leave as UTC since we're just sorting by it?
+    #   @trips = @traveler.trips.where('trip_purpose_id = ?', @time_filter_type).sort_by {|x| x.trip_datetime }.reverse
+    # end
+    @trips = User.find(params[:user_id]).trips
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @trips }
