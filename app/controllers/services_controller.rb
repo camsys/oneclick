f4a225ebb072deeea767a679f1af4585de14291e
dedwards8 <dedwards8@gmail.com>
2014-06-17 14:15:03 -0400

    initial commit

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -18,22 +18,23 @@ class ServicesController < ApplicationController
     @contact = @service.internal_contact
     
     polylines = {}
-    ['origin', 'destination', 'residence'].each do |rule|
-      coverages = @service.service_coverage_maps.where(rule: rule).type_polygon.first
-      polylines[rule] = []
-      if coverages
-        geometry = Boundary.find(3).geom
-        polylines[rule] << {
-          "id" => 0,
-          "geom" => geometry,
-          "options" =>  {"color" => 'red', "width" => "5"}
-        }
-      end
-    end
+
+
+    polylines['origin'] = []
+    geometry = @service.origin
+    geometry = [[33.765449,-84.449358], [33.950195,-84.27495]]
+    polylines['origin'] << {
+      "id" => 0,
+      "geom" => geometry,
+      "options" =>  {"color" => 'red', "width" => "5"}
+    }
+
     @polylines = {}
     @polylines['origin'] = polylines['origin'].to_json || nil
     @polylines['destination'] = polylines['destination'].to_json || nil
     @polylines['residence'] = polylines['residence'].to_json || nil
+
+    puts @polylines.ai
 
     @eh = EligibilityService.new
     respond_to do |format|
