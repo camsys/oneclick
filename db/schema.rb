9b26549e6f4703a8503edc2dda3cf7218261ed93
Aaron Magil <amagil@camsys.com>
2014-06-19 15:24:39 -0400

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -344,10 +344,10 @@ ActiveRecord::Schema.define(version: 20140616211553) do
     t.string   "internal_contact_email"
     t.string   "internal_contact_title"
     t.string   "internal_contact_phone"
-    t.string   "logo_url"
     t.spatial  "origin",                       limit: {:srid=>0, :type=>"geometry"}
     t.spatial  "destination",                  limit: {:srid=>0, :type=>"geometry"}
     t.spatial  "residence",                    limit: {:srid=>0, :type=>"geometry"}
+    t.string   "logo_url"
   end
 
   create_table "services_users", id: false, force: true do |t|
