de77717db5531c95c6985d3ecfed44d8791ede81
denishaskin <denis@constantorbit.com>
2014-06-24 16:22:02 -0700

    FIx bug with earlier commit.

--- lib/trip_planner.rb
+++ lib/trip_planner.rb
@@ -252,7 +252,6 @@ class TripPlanner
 
   def get_drive_time arrive_by, trip_time, from_lat, from_lon, to_lat, to_lon
     tp = TripPlanner.new
-    arrive_by = !is_depart
     result, response = get_fixed_itineraries([from_trip_place.location.first, from_trip_place.location.last],
                                                 [to_trip_place.location.first, to_trip_place.location.last], trip_time, arrive_by.to_s, 'CAR')
     response['itineraries'].first['duration']
