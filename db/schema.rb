c11e686fc0eddb5dc752fc7034eeaaa5e13d3778
Xudong Liu <xudongliu@camsys.com>
2014-06-30 19:14:55 -0400

    [#74184734] add logo in characteristics and accommodations tables; display logo on Options page for each question

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621211528) do
+ActiveRecord::Schema.define(version: 20140630171820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140621211528) do
     t.string  "code"
     t.integer "sequence",                         default: 0
     t.boolean "ask_early",                        default: true
+    t.string  "logo_url"
   end
 
   create_table "agencies", force: true do |t|
@@ -72,6 +73,7 @@ ActiveRecord::Schema.define(version: 20140621211528) do
     t.string  "desc",                              default: ""
     t.integer "sequence",                          default: 0
     t.boolean "ask_early",                         default: true
+    t.string  "logo_url"
   end
 
   create_table "counties", force: true do |t|
@@ -240,6 +242,19 @@ ActiveRecord::Schema.define(version: 20140621211528) do
     t.string  "internal_contact_email", limit: 128
     t.string  "logo_url"
   end
+
+  create_table "rates", force: true do |t|
+    t.integer  "rater_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.integer  "stars",         null: false
+    t.string   "dimension"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "rates", ["rateable_id", "rateable_type"], :name => "index_rates_on_rateable_id_and_rateable_type"
+  add_index "rates", ["rater_id"], :name => "index_rates_on_rater_id"
 
   create_table "ratings", force: true do |t|
     t.integer  "user_id"
