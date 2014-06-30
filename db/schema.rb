5fc8d964266571ccd8fc5c1db6e9df0e4052a1ac
Xudong Liu <xudongliu@camsys.com>
2014-06-30 19:22:48 -0400

    schema update - deleting Rates table

--- db/schema.rb
+++ db/schema.rb
@@ -243,19 +243,6 @@ ActiveRecord::Schema.define(version: 20140630171820) do
     t.string  "logo_url"
   end
 
-  create_table "rates", force: true do |t|
-    t.integer  "rater_id"
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.integer  "stars",         null: false
-    t.string   "dimension"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  add_index "rates", ["rateable_id", "rateable_type"], :name => "index_rates_on_rateable_id_and_rateable_type"
-  add_index "rates", ["rater_id"], :name => "index_rates_on_rater_id"
-
   create_table "ratings", force: true do |t|
     t.integer  "user_id"
     t.integer  "rateable_id"
