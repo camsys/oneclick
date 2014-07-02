d7583d67a8bfbd88a994ee8b0e5c4d9b49b9ba45
Xudong Liu <xudongliu@camsys.com>
2014-07-02 10:04:47 -0400

    [#74184734] modify radio_buttons wrapper css via jQuery to render as button-groups

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -163,6 +163,21 @@ SimpleForm.setup do |config|
     end
   end
 
+  # currently only for trip planning Options page (for ARC only - use button-groups instead of radio-buttons to render boolean options)
+  config.wrappers :h_opts_radio_button_groups, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+    b.use :html5
+    b.use :placeholder
+
+    b.use :label, class: 'col-sm-7 control-label'
+
+    b.wrapper tag: 'div', class: 'col-sm-5 btn-group' do |ba|
+      ba.use :input
+
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    end
+  end
+
   # ==========================
   # For plan a trip page:
 
