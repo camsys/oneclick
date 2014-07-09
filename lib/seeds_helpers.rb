camsys/oneclick

commit e19ff51723cc71d72470857e55b6ee6467e9b98c

Generalizes seeds and sample data to support arbitrary languages.  Removes concrete references to en and es

lib/seeds_helpers.rb
@@ -17,11 +17,14 @@ module SeedsHelpers
       record[:code] = p[:code]
       p[:xlate].each do |k,v|
         translation_fkey = "#{p[:code]}_#{k}"
-        Translation.find_or_create_by!(key: translation_fkey, locale: "en") do |t|
-          t.value = v
-        end
-        Translation.find_or_create_by!(key: translation_fkey, locale: "es") do |t|
-          t.value = "[es]#{v}[/es]"
+        I18n.available_locales.each do |locale|
+          Translation.find_or_create_by!(key: translation_fkey, locale: "es") do |t|
+            if locale.eql? :en
+              t.value = v
+            else
+              t.value = "[#{locale}]#{v}[/#{locale}]"
+            end
+          end
         end
         record[k] = translation_fkey
       end

db/arc/arc_sample_data.rb
@@ -611,7 +611,7 @@ def add_companion
 end
 
 def setup_cms
-    %w{en es}.each do |locale|
+    I18n.available_locales.each do |locale|
       Translation.where(key: 'splash', locale: locale).first_or_create(value: File.open(File.join('db', 'arc', 'splash.html')).read)
     end
 end

db/broward/broward_sample_data.rb
@@ -554,7 +554,7 @@ end
 
 
 def setup_cms
-    %w{en es}.each do |locale|
+    I18n.available_locales.each do |locale|
       Translation.where(key: 'splash', locale: locale).first_or_create(value: File.open(File.join('db', 'broward', 'splash_' + locale + '.html')).read)
     end
 end

db/common_seeds.rb
@@ -29,8 +29,9 @@ end
   Report.find_or_create_by!(rep)
   Translation.find_or_create_by!(key: rep[:class_name], locale: :en,
                                  value: rep[:name] + " Report")
-  Translation.find_or_create_by!(key: rep[:class_name], locale: :es,
-                                 value: "[es]#{rep[:name]} Report[/es]")
+  I18n.available_locales.reject!{|x| x == :en}.each do |l|
+    Translation.find_or_create_by!(key: rep[:class_name], locale: l, value: "[#{l}]#{rep[:name]} Report[/#{l}]")
+  end
 end
 
 # Create Admin User

db/pa/pa_sample_data.rb
@@ -544,7 +544,7 @@ def add_urls_to_pa
 end
 
 def setup_cms
-    %w{en es}.each do |locale|
+    I18n.available_locales.each do |locale|
       Translation.where(key: 'splash', locale: locale).first_or_create(value: File.open(File.join('db', 'pa', 'splash.html')).read)
     end
 end

commit 5e993d90f6122742968f3ee4b3ccb91b7e972aa0

Fixes bug introduced in e19ff51723 where only spanish language translations were created

lib/seeds_helpers.rb
@@ -18,7 +18,7 @@ module SeedsHelpers
       p[:xlate].each do |k,v|
         translation_fkey = "#{p[:code]}_#{k}"
         I18n.available_locales.each do |locale|
-          Translation.find_or_create_by!(key: translation_fkey, locale: "es") do |t|
+          Translation.find_or_create_by!(key: translation_fkey, locale: locale) do |t|
             if locale.eql? :en
               t.value = v
             else