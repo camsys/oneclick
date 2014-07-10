c8f1a49c9ebb28bf1f2a9bc84d1e76733a9db45b
W. Scott Meeks <smeeks@camsys.com>
2014-07-10 16:16:02 -0400

    Unscope Report.find_or_create_by so all existing reports are updated.

--- db/common_seeds.rb
+++ db/common_seeds.rb
@@ -29,7 +29,7 @@ end
   # Need to correctly handle updating active state; match everything except that.
   is_active = rep[:active]
   rep.delete :active
-  report = Report.find_or_create_by!(rep)
+  report = Report.unscoped.find_or_create_by!(rep)
   report.update_attributes(active: is_active)
   Translation.find_or_create_by!(key: rep[:class_name], locale: :en,
                                  value: rep[:name] + " Report")
