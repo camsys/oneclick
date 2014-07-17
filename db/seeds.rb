378b63222081b176454bce4f30d7f1482070fc90
Quinn Shanahan <q.shanahan@gmail.com>
2014-07-17 14:57:39 -0400

    add kiosk location for York

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,17 +14,29 @@ else
   raise "Brand #{Oneclick::Application.config.brand} not handled"
 end
 
-KioskLocation.create!([{
-  name: 'machine1',
-  address_type: 6,
-  addr: '828 Mitchell Street Southwest, Atlanta, GA 30314',
-  lat: 33.7532,
-  lon:  -84.4146
-},
-{
-  name: 'machine2',
-  address_type: 6,
-  addr: '2891 Lakewood Avenue Southwest, Atlanta, GA 30315',
-  lat: 33.6973,
-  lon: -84.4113
-}])
+
+case Oneclick::Application.config.brand
+when 'pa'
+  KioskLocation.create!([{
+    name: 'machine1',
+    address_type: 6,
+    addr: '1230 Roosevelt Ave, York, PA 17404',
+    lat: 39.975886,
+    lon: -76.756512
+  }])
+else
+  KioskLocation.create!([{
+    name: 'machine1',
+    address_type: 6,
+    addr: '828 Mitchell Street Southwest, Atlanta, GA 30314',
+    lat: 33.7532,
+    lon: -84.4146
+  },
+  {
+    name: 'machine2',
+    address_type: 6,
+    addr: '2891 Lakewood Avenue Southwest, Atlanta, GA 30315',
+    lat: 33.6973,
+    lon: -84.4113
+  }])
+end
