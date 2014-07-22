a4de5ed7e1a8312e4eba130d271fadef2310d878
denishaskin <denis@constantorbit.com>
2014-07-22 10:13:33 -0400

    Update honeybadger API key for JTA [Fixes #75437686]

--- config/initializers/_oneclick.rb
+++ config/initializers/_oneclick.rb
@@ -155,7 +155,7 @@ when 'jta'
   Oneclick::Application.config.agent_read_feedback = true
   Oneclick::Application.config.provider_read_all_feedback = true
   Oneclick::Application.config.tripless_feedback = false
-  honeybadger_api_key = 'f49faffa'
+  honeybadger_api_key = '0447225c'
   Oneclick::Application.config.poi_file = 'db/jta/locations.csv'
   Oneclick::Application.config.default_county = 'Duval'
   Oneclick::Application.config.state = 'FL'
