059a23ac29a0a82563e2daca8bf494c809acbb89
dedwards8 <dedwards8@gmail.com>
2014-08-14 22:09:13 -0400

    [Fixes#75388674] Excludes endpoint_areas and coverage_areas from the Services screen

--- app/models/geo_coverage.rb
+++ app/models/geo_coverage.rb
@@ -7,6 +7,10 @@ class GeoCoverage < ActiveRecord::Base
   #coverage_type: zipcode, county_name, polygon, city
   # attr_accessible :coverage_type, :value, :polygon
 
+  scope :not_endpoint, -> { where.not(coverage_type: 'endpoint_area') }
+  scope :not_coverage, -> { where.not(coverage_type: 'coverage_area') }
+
+
   def polygon_contains?(lon, lat)
     factory = RGeo::Geographic.simple_mercator_factory
     point = factory.point(lon.to_f, lat.to_f)
