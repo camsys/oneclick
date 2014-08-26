9b8fa4cc8a35d0da3087af7191cf7d02666257a5
dedwards8 <dedwards8@gmail.com>
2014-08-26 10:35:33 -0700

    Temporaily changes application.rb time zone to Pacific

--- config/application.rb
+++ config/application.rb
@@ -66,7 +66,7 @@ module Oneclick
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # Set in _oneclick.rb because can be brand-dependent
-    config.time_zone = 'Eastern Time (US & Canada)'
+    config.time_zone = 'Pacific Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
