f4bd1310c852a2abdb3c649340f9dea7b1275ba5
Xudong Liu <xudongliu@camsys.com>
2014-09-15 16:27:20 -0400

    remove the test aginst translation table count as its too brittle to be part of a test

--- spec/controllers/admin/translations_controller_spec.rb
+++ spec/controllers/admin/translations_controller_spec.rb
@@ -10,8 +10,6 @@ describe Admin::TranslationsController do
     it "should provide a list of translations" do
       get :index
       response.should be_success
-      # TODO Brittle test; dependent on db seeding
-      assigns(:translations_proxies).count.should eql(914)
       assigns(:translations_proxies).first.should be_a(TranslationProxy)
     end
   end
