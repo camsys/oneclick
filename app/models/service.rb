281fd9020d66d9829709aa21c14e0c081eef2186
Xudong Liu <xudongliu@camsys.com>
2014-09-17 11:21:16 -0400

    [fixes#74336366]

--- app/models/service.rb
+++ app/models/service.rb
@@ -170,37 +170,37 @@ class Service < ActiveRecord::Base
 
   def get_shapefile_first_geometry(shapefile_path)
     unless shapefile_path.nil?
-      #begin
-      Zip::File.open(shapefile_path) do |zip_file|
-        zip_shp = zip_file.glob('*/*.shp').first
-        unless zip_shp.nil?
-          zip_shp_paths = zip_shp.name.split('/')
-          file_name = zip_shp_paths[zip_shp_paths.length - 1].sub '.shp', ''
-          shp_name = nil
-          Dir.mktmpdir do |dir|
-            shp_name = "#{dir}/" + file_name + '.shp'
-            zip_file.each do |entry|
-              entry_names = entry.name.split('/')
-              entry_name = entry_names[entry_names.length - 1]
-              if entry_name.include?(file_name)
-                entry.extract("#{dir}/" + entry_name)
+      begin
+        Zip::File.open(shapefile_path) do |zip_file|
+          zip_shp = zip_file.glob('**/*.shp').first
+          unless zip_shp.nil?
+            zip_shp_paths = zip_shp.name.split('/')
+            file_name = zip_shp_paths[zip_shp_paths.length - 1].sub '.shp', ''
+            shp_name = nil
+            Dir.mktmpdir do |dir|
+              shp_name = "#{dir}/" + file_name + '.shp'
+              zip_file.each do |entry|
+                entry_names = entry.name.split('/')
+                entry_name = entry_names[entry_names.length - 1]
+                if entry_name.include?(file_name)
+                  entry.extract("#{dir}/" + entry_name)
+                end
               end
-            end
 
-            RGeo::Shapefile::Reader.open(shp_name, { :assume_inner_follows_outer => true }) do |shapefile|
-              shapefile.each do |shape|
-                if not shape.geometry.nil? and shape.geometry.geometry_type.to_s.downcase.include?('polygon') #only return first polygon
-                  return shape.geometry
+              RGeo::Shapefile::Reader.open(shp_name, { :assume_inner_follows_outer => true }) do |shapefile|
+                shapefile.each do |shape|
+                  if not shape.geometry.nil? and shape.geometry.geometry_type.to_s.downcase.include?('polygon') #only return first polygon
+                    return shape.geometry
+                  end
                 end
               end
             end
           end
         end
+      rescue Exception => msg
+        Rails.logger.info 'shapefile parse error'
+        Rails.logger.info msg
       end
-      #rescue Exception => msg
-      #  Rails.logger.info 'shapefile parse error'
-      #  Rails.logger.info msg
-      #end
     end
 
     return nil
