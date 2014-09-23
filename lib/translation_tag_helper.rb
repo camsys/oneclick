ae316909151750ef8ceda415644a8cd124daab2c
Xudong Liu <xudongliu@camsys.com>
2014-09-23 14:04:16 -0400

    [#79018378] change default_locale as tags when in tags locale, after translation, restore prev default_locale

--- lib/translation_tag_helper.rb
+++ lib/translation_tag_helper.rb
@@ -1,7 +1,13 @@
 module TranslationTagHelper
 
   def translate_w_tag_as_default tag
-    I18n.translate(tag.to_sym, default: '[' + tag.to_s + ']')
+  	is_in_tags = I18n.locale == :tags
+  	current_default_locale = I18n.default_locale
+  	I18n.default_locale = :tags if is_in_tags
+    locale_text = I18n.translate(tag.to_sym, default: '[' + tag.to_s + ']')
+  	I18n.default_locale = current_default_locale if is_in_tags
+
+  	locale_text
   end
 
 end
