81ab0bc33eff3f1784c01dbc25f44dc9ce736390
Xudong Liu <xudongliu@camsys.com>
2014-10-01 16:57:37 -0400

    [fixes#79448796] update arc eligibility questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140926204615) do
+ActiveRecord::Schema.define(version: 20141001205018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
