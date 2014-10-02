51de47add77b67d83442aa5aa8552f3bb3be9402
dedwards8 <dedwards8@gmail.com>
2014-10-02 11:04:48 -0400

    [Fixes#79977658] Unable to plan a trip when assisting.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  #before_filter :authenticate_user!
+  before_filter :authenticate_user!
   load_and_authorize_resource except: [:edit, :assist]
 
   def index
