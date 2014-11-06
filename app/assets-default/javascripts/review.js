1e5b170e702992039460a7450c515bf86e40f01c
Austin Winslow <awinslow@camsys.com>
2014-11-06 11:19:59 -0500

    Switch itinerary tooltips for popovers [#82041242]
    Conflicts:
    	app/assets-default/javascripts/review.js

--- app/assets-default/javascripts/review.js
+++ app/assets-default/javascripts/review.js
@@ -312,10 +312,26 @@ function TripReviewPageRenderer(intervalStep, barHeight, tripResponse, filterCon
 
         $('.ui-slider-handle').empty();
 
-        $(".single-plan-chart-container, .trip-mode-cost").tooltip({
+        $(".single-plan-chart-container, .trip-mode-cost").popover({
             'html': true,
             'container': 'body',
-            'trigger': 'hover focus'
+            'trigger': 'manual',
+            'animation': false,
+            'placement': 'top'
+        })
+        .on("mouseenter", function () {
+          var _this = this;
+          $(this).popover("show");
+          $(".popover").on("mouseleave", function () {
+              $(_this).popover('hide');
+          });
+        }).on("mouseleave", function () {
+          var _this = this;
+          setTimeout(function () {
+              if (!$(".popover:hover").length) {
+                  $(_this).popover("hide");
+              }
+          }, 300);
         });
 
         $('.label-help').addClass('fa-2x').tooltip({ 'html': true, 'container': 'body', 'trigger': 'hover focus' });
