5458fb81e20c379814d4a24b554072841e74e771
Xudong Liu <xudong.camsys@gmail.com>
2015-04-07 09:37:37 -0400

    removed a mistaken-checked-in migration file

--- db/migrate/20150401145452_drop_columns_from_filter_fields.rb
+++
@@ -1,7 +0,0 @@
-class DropColumnsFromFilterFields < ActiveRecord::Migration
-  def change
-    remove_column :reporting_filter_fields, :is_filterable, :boolean
-    remove_column :reporting_filter_fields, :is_output, :boolean
-    remove_column :reporting_filter_fields, :is_validate, :boolean
-  end
-end
