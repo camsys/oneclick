307e96ce26197248a5f2f319b91979b68d71ab2d
Xudong Liu <xudong.camsys@gmail.com>
2015-04-21 18:13:23 -0400

    removed exception catch for reporting arc-qa debugging

--- app/controllers/reporting/results_controller.rb
+++ app/controllers/reporting/results_controller.rb
@@ -13,40 +13,32 @@ module Reporting
       @q = @report.data_model.ransack q_param
       @params = {q: q_param}
 
-      begin
-        # list all output fields
-        # if output_fields is empty, then export all columns in this table
-        @fields = @report.reporting_output_fields.blank? ?
-          @report.data_model.column_names.map{
-            |x| {
-              name: x 
-            }
-          } : @report.reporting_output_fields
+      # list all output fields
+      # if output_fields is empty, then export all columns in this table
+      @fields = @report.reporting_output_fields.blank? ?
+        @report.data_model.column_names.map{
+          |x| {
+            name: x 
+          }
+        } : @report.reporting_output_fields
 
-        # default order by :id
-        if !@report.data_model.columns_hash.keys.index("id").nil? 
-          @q.sorts = "id asc" if @q.sorts.empty?
-        end
-
-        # total_results is for exporting
-        total_results = @q.result(:district => true)
-
-        # filter data based on accessibility
-        total_results = filter_data(total_results)
-        
-        # @results is for html display; only render current page
-        @results = total_results.order(:id)  if !@report.data_model.columns_hash.keys.index("id").nil?
-        @results = total_results.page(page).per(@per_page)
-
-        # this is used to test if any sql exception is triggered in querying
-        # commen errors: table not found
-        first_result = @results.limit(1) 
-
-      rescue => e
-        # error message handling
-        total_results = []
-        @results = []
+      # default order by :id
+      if !@report.data_model.columns_hash.keys.index("id").nil? 
+        @q.sorts = "id asc" if @q.sorts.empty?
       end
+
+      # total_results is for exporting
+      total_results = @q.result(:district => true)
+
+      # filter data based on accessibility
+      total_results = filter_data(total_results)
+      
+      # @results is for html display; only render current page
+      @results = total_results.page(page).per(@per_page)
+
+      # this is used to test if any sql exception is triggered in querying
+      # commen errors: table not found
+      first_result = @results.limit(1) 
 
       respond_to do |format|
         format.html
