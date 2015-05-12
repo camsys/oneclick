8497a8b51b46942f7d72d8bc1d6beefa0bff2646
Alex Bromley <evilbikes79@gmail.com>
2015-05-12 13:52:51 -0400

    Allow taxi itineraries to build even when there is no user available.

--- app/models/taxi_itinerary.rb
+++ app/models/taxi_itinerary.rb
@@ -10,19 +10,19 @@ class TaxiItinerary < Itinerary
     end
   end
 
-  def self.get_taxi_itineraries(from, to, trip_datetime)
+  def self.get_taxi_itineraries(from, to, trip_datetime, trip_user)
 
     itineraries = []
 
     taxi_mode = Mode.find_by_code("mode_taxi")
     taxi_services = Service.where("mode_id = ?", taxi_mode.id)
 
-    user = User.find(1)
-
     api_key = Oneclick::Application.config.taxi_fare_finder_api_key
 
     taxi_services.each do |taxi_service|
-      if (taxi_service.is_valid_for_trip_area(from, to) && taxi_service.can_provide_user_accommodations(user, taxi_service))
+      taxi_service_match = taxi_service.is_valid_for_trip_area(from, to)
+      taxi_service_match = taxi_service.can_provide_user_accommodations(trip_user, taxi_service) if (trip_user.present? && taxi_service_match)
+      if (taxi_service_match)
         city = taxi_service.taxi_fare_finder_city
         if city.present?
           results = TaxiRestService.call_out_to_taxi_fare_finder(city, api_key, from, to)
@@ -36,7 +36,7 @@ class TaxiItinerary < Itinerary
         new_itinerary.mode = taxi_mode
         new_itinerary.service = taxi_service
         new_itinerary.calculate_fare
-        
+
         itineraries.push(new_itinerary)
       end
     end
