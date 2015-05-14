1e07eac8b8606087c0a07ce69d7626499d41528d
Sathyan1983 <Sathyan.Mathai@gmail.com>
2015-05-14 14:37:28 -0400

    wrote and ran migration to add disabled_comment to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514181211) do
+ActiveRecord::Schema.define(version: 20150514183456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -729,6 +729,7 @@ ActiveRecord::Schema.define(version: 20150514181211) do
     t.integer  "walking_maximum_distance_id"
     t.datetime "deleted_at"
     t.integer  "maximum_wait_time"
+    t.string   "disable_comment"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token"
