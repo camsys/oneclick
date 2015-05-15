04387eacdac7e42128f13b79776d9a64d0719c03
dedwards8 <dedwards8@gmail.com>
2015-05-15 13:56:23 -0400

    Add schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512153614) do
+ActiveRecord::Schema.define(version: 20150515175117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20150512153614) do
     t.boolean "active",                             default: true, null: false
     t.text    "private_comments_old"
     t.text    "public_comments_old"
+    t.string  "token"
   end
 
   create_table "agency_user_relationships", force: true do |t|
@@ -135,8 +136,8 @@ ActiveRecord::Schema.define(version: 20150512153614) do
     t.string   "zone_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "service_id"
     t.spatial  "geom",       limit: {:srid=>0, :type=>"geometry"}
+    t.integer  "service_id"
   end
 
   add_index "fare_zones", ["service_id"], :name => "index_fare_zones_on_service_id"
@@ -199,6 +200,10 @@ ActiveRecord::Schema.define(version: 20150512153614) do
     t.boolean  "too_early",                                         default: false
     t.string   "returned_mode_code"
     t.text     "order_xml"
+    t.boolean  "assistant"
+    t.integer  "companions"
+    t.integer  "children"
+    t.integer  "other_passengers"
   end
 
   create_table "kiosk_locations", force: true do |t|
@@ -521,10 +526,10 @@ ActiveRecord::Schema.define(version: 20150512153614) do
     t.text     "public_comments_old"
     t.text     "private_comments_old"
     t.string   "logo"
+    t.integer  "max_advanced_book_minutes",                default: 0,     null: false
     t.string   "display_color"
     t.integer  "mode_id"
     t.string   "taxi_fare_finder_city",        limit: 64
-    t.integer  "max_advanced_book_minutes",                default: 20160, null: false
   end
 
   create_table "services_users", id: false, force: true do |t|
@@ -639,6 +644,7 @@ ActiveRecord::Schema.define(version: 20150512153614) do
     t.string   "kiosk_code"
     t.string   "token"
     t.boolean  "is_planned",                           default: false
+    t.string   "agency_token"
   end
 
   create_table "trips_desired_modes", force: true do |t|
