88dead0e0a3c9157f75ebc4fa257771cbeabf029
Xudong Liu <xudong.camsys@gmail.com>
2015-05-19 16:40:13 -0400

    add conditional check on add_column for reporting_reports table

--- db/migrate/20150422175448_add_primary_key_to_reporting_reports.rb
+++ db/migrate/20150422175448_add_primary_key_to_reporting_reports.rb
@@ -1,5 +1,7 @@
 class AddPrimaryKeyToReportingReports < ActiveRecord::Migration
   def change
-    add_column :reporting_reports, :primary_key, :string, null: false, default: 'id'
+    unless column_exists? :reporting_reports, :primary_key
+      add_column :reporting_reports, :primary_key, :string, null: false, default: 'id'
+    end
   end
 end
