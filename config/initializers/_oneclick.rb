f945617d5a72044bf47b71a74bb2e2dd840502c0
Xudong Liu <xudong.camsys@gmail.com>
2015-05-19 12:55:24 -0400

    correct geocoder_bounds to make sure it follows [[xmin, ymin], [xmax, ymax]] format

--- config/initializers/_oneclick.rb
+++ config/initializers/_oneclick.rb
@@ -129,7 +129,7 @@ Oneclick::Application.configure do
     config.tablet_favicon = ''
     config.geocoder_components = 'country:US'
     config.map_bounds = [[26.427309, -80.347081], [25.602294, -80.061728]]
-    config.geocoder_bounds = [[26.427309, -80.347081], [25.602294, -80.061728]]
+    config.geocoder_bounds = [[25.602294, -80.347081], [26.427309, -80.061728]]
     config.open_trip_planner = "http://otp-fl.camsys-apps.com:8080/otp/routers/fl/plan?"
     config.transit_respects_ada = false
     config.taxi_fare_finder_api_key = "SIefr5akieS5"
@@ -165,7 +165,7 @@ Oneclick::Application.configure do
     config.geocoder_components = 'country:US'
     # TODO Do we maybe need different bounds for kiosk vs. default?
     config.map_bounds      = [[40.0262999543423,  -76.56372070312499], [39.87970800405549, -76.90189361572266]]
-    config.geocoder_bounds = [[40.0262999543423,  -76.56372070312499], [39.87970800405549, -76.90189361572266]]
+    config.geocoder_bounds = [[39.87970800405549,  -76.90189361572266], [40.0262999543423, -76.56372070312499]]
     config.default_zoom = 12
     config.open_trip_planner = "http://otp-pa.camsys-apps.com:8080/otp/routers/pa/plan?"
     config.transit_respects_ada = false
@@ -253,7 +253,7 @@ Oneclick::Application.configure do
     config.geocoder_components = 'country:US'
     # TODO Do we maybe need different bounds for kiosk vs. default?
     config.map_bounds      = [[33.163,-117.874],[36.053,-114.033]]
-    config.geocoder_bounds = [[33.163,-117.874],[36.063,-114.033]]
+    config.geocoder_bounds = [[36.063,-117.874],[33.163,-114.033]]
     config.default_zoom = 12
     config.open_trip_planner = "http://otp-ieuw.camsys-apps.com:8080/otp/routers/ieuw/plan?"
     config.transit_respects_ada = false
