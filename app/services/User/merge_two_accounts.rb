1b18a083b776984195d8f1a44b9f39474fc2afcd
Sathyan1983 <Sathyan.Mathai@gmail.com>
2015-05-21 16:08:54 -0400

    Edited service object for merging accounts, but have not resolved issue of creating excessive duplicates.

--- app/services/User/merge_two_accounts.rb
+++ app/services/User/merge_two_accounts.rb
@@ -11,22 +11,27 @@ class User
 
     private
 
-    def initialize
+    def initialize(user1, user2)
       @main = user1
       @sub = user2
       @reflections = User.reflect_on_all_associations
+      merge_all_possible
     end
 
     def merge_all_possible
-      @reflections.each do |r| { |r| merge_association(r) if mergeable?(r) }
+      @reflections.each { |r| merge_association(r) if mergeable?(r) && exists(r) }
     end
 
     def mergeable?(r)
       r.macro == :has_many || r.macro == :has_and_belongs_to_many
     end
 
+    def exists(r)
+       r.name != :multi_o_d_trips
+    end
+
     def merge_association(r)
-      @main.send(r.name) += @sub.send(r.name)
+      @sub.send(r.name).each { |assoc| @main.send(r.name) << assoc.dup }
     end
 
   end
