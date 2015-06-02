5132e12a84b2cdee1919862c4c0d27e64d5ecb19
Alex Bromley <evilbikes79@gmail.com>
2015-06-02 10:04:05 -0400

    replacing code too tightly coupled to translations [#94010212]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -196,8 +196,9 @@ module ApplicationHelper
 
     start_days = start_time_in_seconds/3600/24.round
     end_days = end_time_in_seconds/3600/24.round
+    #count: end_days
 
-    start_days.to_s + " " + TranslationEngine.translate_text(:to).downcase + " " + TranslationEngine.translate_text(:day, count: end_days)
+    start_days.to_s + " " + TranslationEngine.translate_text(:to).downcase + " " + TranslationEngine.translate_text(:day)
   end
 
   def get_boolean(val)
