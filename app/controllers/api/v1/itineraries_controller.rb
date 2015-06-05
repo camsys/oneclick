d5962d110bd5cf6f9fbfb00b8070fcee76930587
dedwards8 <dedwards8@gmail.com>
2015-06-05 09:32:33 -0400

    [Fixes#95716960] DO/PU times are returned after a successful booking request.

--- app/controllers/api/v1/itineraries_controller.rb
+++ app/controllers/api/v1/itineraries_controller.rb
@@ -158,7 +158,9 @@ module Api
         if booked_itineraries.count > 0
           booked_itineraries.each do |bi|
             status  = bi.status
-            results_array.append({trip_id: bi.trip_part.trip.id, itinerary_id: bi.id, success: true, confirmation_id: bi.booking_confirmation, negotiated_pu_time: status[1][:pu_time], negotiated_do_time: status[1][:do_time] })
+            negotiated_pu_time = status[1][:pu_time].nil? ? nil : status[1][:pu_time].to_time.iso8601
+            negotiated_do_time = status[1][:do_time].nil? ? nil : status[1][:do_time].to_time.iso8601
+            results_array.append({trip_id: bi.trip_part.trip.id, itinerary_id: bi.id, success: true, confirmation_id: bi.booking_confirmation, negotiated_pu_time: negotiated_pu_time, negotiated_do_time: negotiated_do_time })
           end
 
         #Build Failure Response
