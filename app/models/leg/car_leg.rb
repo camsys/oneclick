4cbe93219bfb7e727bba4763d45ad5e91de30b4f
Alex Bromley <evilbikes79@gmail.com>
2015-06-09 10:53:09 -0700

    WIP refactor trip legs [#95627618]

--- app/models/leg/car_leg.rb
+++ app/models/leg/car_leg.rb
@@ -1,22 +1,18 @@
-#
-# Concrete implementation of a walking leg
-#
-module Leg
-  class CarLeg < TripLeg
+class CarLeg < Leg
 
-    def initialize(attrs = {})
+  def initialize(attrs = {})
 
-      super(attrs)
-      attrs.each do |k, v|
-        self.send "#{k}=", v
-      end
-
-      self.mode = CAR
-
+    super(attrs)
+    attrs.each do |k, v|
+      self.send "#{k}=", v
     end
 
-    def short_description
-      [TranslationEngine.translate_text(:drive_or_taxi), TranslationEngine.translate_text(:to), end_place.name].join(' ')
-    end
+    self.mode = CAR
+
   end
+
+  def short_description
+    [TranslationEngine.translate_text(:drive_or_taxi), TranslationEngine.translate_text(:to), end_place.name].join(' ')
+  end
+
 end
