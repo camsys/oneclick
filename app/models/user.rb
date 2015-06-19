3077d0be70d22006ca19cc3ee14cec15030ec6e5
Xudong Liu <xudong.camsys@gmail.com>
2015-06-19 16:10:44 -0400

    [#95424584] using DateTime.now as current time to compare if within delivery date range

--- app/models/user.rb
+++ app/models/user.rb
@@ -255,7 +255,7 @@ class User < ActiveRecord::Base
 
   def unread_received_messages
     received_messages.where(read: false)
-      .joins(:message).where('messages.from_date is ? or messages.from_date <= ?',nil,Date.today)
-      .where('messages.to_date is ? or messages.to_date >= ?',nil,Date.today)
+      .joins(:message).where('messages.from_date is ? or messages.from_date <= ?',nil,DateTime.now)
+      .where('messages.to_date is ? or messages.to_date >= ?',nil,DateTime.now)
   end
 end
