73ea1f510acd4c361d5fdd6f102f89fdaedbd442
Sathyan1983 <Sathyan.Mathai@gmail.com>
2015-06-19 10:09:10 -0400

    preparing to fix the bug

--- app/services/User/find_user_join_tables.rb
+++ app/services/User/find_user_join_tables.rb
@@ -1,7 +1,9 @@
 # This service object will return a hash in which the name of each
 # join table associated with the user model is a key that points to 'true'
 # Join tables should not be reassigned because the objects inside them are
-# already reassigned.
+# already reassigned.  The only exception is when a user 'has_many' through
+# another class that has_many of the association.  For example, this object
+# should not reassign trip_places because it should reassign the trips instead.
 
 class User
 
@@ -18,8 +20,15 @@ class User
     def self.find_keys
       reflections = User.reflect_on_all_associations
       throughs = reflections.select { |r| r.name != :multi_od_trips && r.options.has_key?(:through) }
-      join_table_names = throughs.inject([]) { |memo, r| memo << r.options[:through] }
+      join_table_names = throughs.inject([]) { |memo, r| memo << r.options[:through] if !self.many_through_many(r) }
       join_table_names
+    end
+
+    def self.many_through_many(r)
+      join_class_string = r.options[:through].to_s.singularize.camelize
+      join_klass = Object::const_get(join_class_string)
+      singular_reflection = r.name.to_s.singularize.to_sym
+      join_klass.reflect_on_association(singular_reflection).macro == :has_many
     end
 
   end
