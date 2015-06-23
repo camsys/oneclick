94af46989f5754ceee454c220bbf77702b201b76
Alex Bromley <evilbikes79@gmail.com>
2015-06-23 14:50:44 -0400

    default to correct locale when adding a new translation

--- app/controllers/admin/translations_controller.rb
+++ app/controllers/admin/translations_controller.rb
@@ -17,7 +17,7 @@ class Admin::TranslationsController < Admin::BaseController
         #same form is used to create new keys as well as new translations with existing keys
         locale = Locale.find_by_name(trans_params["locale"])
         translation_key = TranslationKey.find_or_create_by!(name: trans_params["key"])
-        @translation = Translation.new()
+        @translation = Translation.new
         @translation.value = trans_params["value"]
         @translation.locale = locale
         @translation.translation_key = translation_key
