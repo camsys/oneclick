3fde83d8e0cff393380119de6c9c2e2853adc5d7
dedwards8 <dedwards8@gmail.com>
2015-07-08 16:42:09 -0400

    Sub spaces for underscores when searching for matching trip purposes.

--- lib/ecolane_helpers.rb
+++ lib/ecolane_helpers.rb
@@ -487,7 +487,7 @@ class EcolaneHelpers
       ecolane_purpose = options.xpath("purpose").text
       simplified_ecolane_purpose = ecolane_purpose.downcase.gsub(%r{[ /]}, '_')
 
-      Rails.logger.info("Comparing Trip Purposes - Ecolane: " + simplified_ecolane_purpose + ', 1-Click: ' + purpose.gsub(%r{[ /]}, '_'))
+      Rails.logger.info("Comparing Trip Purposes - Ecolane: " + simplified_ecolane_purpose + ', 1-Click: ' + purpose.downcase.gsub(%r{[ /]}, '_'))
 
       if simplified_ecolane_purpose == 'other' or simplified_ecolane_purpose == purpose.gsub(%r{[ /]}, '_')
         funding_source = options.xpath("funding_source").text
