1ff68b68940b4bc52736cdb03898aecdac51dbc0
Derek Edwards <dedwards8@gmail.com>
2015-12-08 16:10:54 -0500

    [Fixes#109624386] Allow users to bookmark pages that include user ids.  When you are creating a new trip with a url from a different user, you will be redirected to the root url

--- app/controllers/traveler_aware_controller.rb
+++ app/controllers/traveler_aware_controller.rb
@@ -1,10 +1,24 @@
 class TravelerAwareController < ApplicationController
-  before_action do |controller|
-    if params[:user_id]
-      get_traveler
-      if params[:user_id] != @traveler.id.to_s && ENV['UI_MODE'] != 'kiosk'
-        raise CanCan::AccessDenied.new
+  before_action :confirm_traveler, :except => [:new]
+  before_action :confirm_traveler_new, :only => [:new]
+
+  private
+    def confirm_traveler
+      if params[:user_id]
+        get_traveler
+        if params[:user_id] != @traveler.id.to_s && ENV['UI_MODE'] != 'kiosk'
+          raise CanCan::AccessDenied.new
+        end
+      end
+    end
+
+    #If you are trying to create a new trip and you aren't the correct user, go to the landing page instead
+    def confirm_traveler_new
+      if params[:user_id]
+        get_traveler
+        if params[:user_id] != @traveler.id.to_s && ENV['UI_MODE'] != 'kiosk'
+          redirect_to root_url
+        end
       end
     end
   end
-end
