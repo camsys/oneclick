f3d37eaefc1a26fbd5553624a50336a99bbf5262
Derek Edwards <dedwards8@gmail.com>
2016-02-02 17:45:32 -0500

    Update Sample Data to run and load home page.

--- db/common_seeds.rb
+++ db/common_seeds.rb
@@ -256,3 +256,4 @@ Rake::Task['oneclick:set_default_logo'].invoke
 Rake::Task['oneclick:set_mode_icons'].invoke
 Rake::Task['oneclick:add_otp_modes'].invoke
 Rake::Task["translation_engine:wipe_and_reload_from_arc_qa_data"].invoke
+Rake::Task["oneclick:add_feedback_types"].invoke
