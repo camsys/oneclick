86c9be28994d2cdf95ef65f101bcc639fd706082
Derek Edwards <dedwards8@gmail.com>
2016-02-03 18:25:42 -0500

    Fixes site periodically loading into spanish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,7 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.locale || I18n.default_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
   def default_url_options(options={}) # This overrides/extends
