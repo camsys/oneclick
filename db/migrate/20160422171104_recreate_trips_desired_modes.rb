ed7ed1ba41c3f53b9da0d587e439ed427aaaa12e
Derek Edwards <dedwards8@gmail.com>
2016-04-22 13:34:01 -0400

    Update schema.

--- db/migrate/20160422171104_recreate_trips_desired_modes.rb
+++ db/migrate/20160422171104_recreate_trips_desired_modes.rb
@@ -1,12 +1,7 @@
 class RecreateTripsDesiredModes < ActiveRecord::Migration
   def change
 
-    drop_table :trips_desired_modes
-
-    create_table :trips_desired_modes, :id => false do |t|
-      t.integer :trip_id, null: false
-      t.integer :desired_mode_id, null: false
-    end
+    remove_column :trips_desired_modes, :id
 
   end
 end
