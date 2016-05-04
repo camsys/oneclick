d3c60eafd923d19f50e5af1adf1a515d642e5073
Derek Edwards <dedwards8@gmail.com>
2016-05-04 12:53:06 -0400

    Allow trips requested in the API specify optimizing tranfers, walking time, or total trip time.  Total trip time is the default.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422200339) do
+ActiveRecord::Schema.define(version: 20160504154430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -835,8 +835,8 @@ ActiveRecord::Schema.define(version: 20160422200339) do
     t.integer  "user_id"
     t.integer  "trip_purpose_id"
     t.integer  "creator_id"
-    t.datetime "created_at",                                           null: false
-    t.datetime "updated_at",                                           null: false
+    t.datetime "created_at",                                            null: false
+    t.datetime "updated_at",                                            null: false
     t.boolean  "taken"
     t.date     "scheduled_date"
     t.datetime "scheduled_time"
