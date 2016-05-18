4da2b4e1ae4e5e6e67ef846b0e60df69b2638e2c
Derek Edwards <dedwards8@gmail.com>
2016-05-18 14:26:45 -0400

    Store Places Exactly as they are received by the API call.

--- app/models/place.rb
+++ app/models/place.rb
@@ -110,56 +110,60 @@ class Place < GeocodedAddress
   end
 
   def build_place_details_hash
-    require 'indirizzo'
     #Based on Google Place Details
-
-    parsable_address = Indirizzo::Address.new(self.address1)
-
     {
-        address_components: [
-        {
-        long_name: parsable_address.number,
-        short_name: parsable_address.number,
-        types: ["street_number"]
-    },
-        {
-        long_name: parsable_address.street.first,
-        short_name: parsable_address.street.first,
-        types: ["route"]
-    },
-        {
-        long_name: self.address1,
-        short_name: self.address1,
-        types: ["street_address"]
-    },
-        {
-            long_name: self.city,
-        short_name: self.city,
-        types: ["locality", "political"]
-    },
-        {
-            long_name: self.state,
-        short_name: self.state,
-        types: ["administrative_area_level_1","political"]
-    },
-        {
-            long_name: self.zip,
-        short_name: self.zip,
-        types: ["postal_code"]
-    }
-    ],
+        address_components: self.address_components,
 
-        formatted_address: self.address,
+        formatted_address: self.raw_address,
+        place_id: self.google_place_id,
         geometry: {
         location: {
         lat: self.lat,
         lng: self.lon,
-    }
+      }
     },
         id: self.id,
         name: self.name,
-        scope: "user"
+        scope: "user",
+        stop_code: self.stop_code
     }
+  end
+
+  def address_components
+    address_components = []
+
+    #street_number
+    if self.street_number
+      address_components << {long_name: self.street_number, short_name: self.street_number, types: ['street_number']}
+    end
+
+    #Route
+    if self.route
+      address_components << {long_name: self.route, short_name: self.route, types: ['route']}
+    end
+
+    #Street Address
+    if self.address1
+      address_components << {long_name: self.address1, short_name: self.address1, types: ['street_address']}
+    end
+
+    #City
+    if self.city
+      address_components << {long_name: self.city, short_name: self.city, types: ["locality", "political"]}
+    end
+
+    #State
+    if self.state
+      address_components << {long_name: self.state, short_name: self.state, types: ["postal_code"]}
+    end
+
+    #Zip
+    if self.state
+      address_components << {long_name: self.zip, short_name: self.zip, types: ["administrative_area_level_1","political"]}
+    end
+
+    return address_components
+
   end
 
 end
