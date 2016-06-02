67cf4cf48676be57f820fc89e893b2751ab3587a
Derek Edwards <dedwards8@gmail.com>
2016-06-02 12:31:33 -0400

    Set headers on response

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -23,6 +23,7 @@ module Api
       def set_access_control_headers
         headers['Access-Control-Allow-Origin'] = '*'
         headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+        headers['Access-Control-Allow-Headers'] = 'Content-Type'
       end
 
       def confirm_user_token
