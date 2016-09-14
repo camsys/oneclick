dbd6d3e8d4ded67fa7b7bdc3714c4117eef9a655
Derek Edwards <dedwards8@gmail.com>
2016-09-14 16:30:53 -0400

    Rename Comments Private.  Not allowed to have a scope named private.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,11 +5,11 @@ class Comment < ActiveRecord::Base
   validates :locale, inclusion: { in: I18n.available_locales.map{|l| l.to_s},
     message: "%{value} is not a valid defined locale" }
   validates :visibility, presence: true
-  validates :visibility, inclusion: { in: %w(my_public private),
-    message: "%{value} must be either my_public or private" }
+  validates :visibility, inclusion: { in: %w(my_public my_private),
+    message: "%{value} must be either my_public or my_private" }
 
   scope :my_public, -> {where(visibility: 'public')}
-  scope :private, -> {where(visibility: 'private')}
+  scope :my_private, -> {where(visibility: 'private')}
 
   def self.for_locale
     where(locale: I18n.locale).first
