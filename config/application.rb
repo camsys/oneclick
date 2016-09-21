1456af4af5350ac5581858b200fc2a553b313006
Derek Edwards <dedwards8@gmail.com>
2016-09-21 16:58:05 -0400

    Heroku compile assets

--- config/application.rb
+++ config/application.rb
@@ -94,6 +94,7 @@ module Oneclick
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.compile=true
     # For heroku; see http://blog.nathanhumbert.com/2012/01/rails-32-on-heroku-tip.html
     config.assets.initialize_on_precompile = false
     config.serve_static_assets = true
