e5cf8a246921a67b3c6f215da62d70743ca7c65b
Derek Edwards <dedwards8@gmail.com>
2016-09-22 16:15:46 -0400

    Dev should send actual emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Oneclick::Application.configure do
 
   config.active_support.deprecation = :log
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
