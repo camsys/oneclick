fb489e02a95983f57e72153b6e42a67ef497c39b
Derek Edwards <dedwards8@gmail.com>
2016-09-25 15:21:42 -0400

    Set better default spanish.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Oneclick::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.cache_classes = false
+  #config.cache_classes = false
 
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
@@ -9,11 +9,11 @@ Oneclick::Application.configure do
   config.action_dispatch.best_standards_support = :builtin
 
   # development settings
-  config.serve_static_assets = true
-  config.assets.compress = false
+  #config.serve_static_assets = true
+  #config.assets.compress = false
   # config.assets.compile
   # config.assets.digest
-  config.assets.debug = true
+  #config.assets.debug = true
 
   # # QA settings
   # config.serve_static_assets = true
