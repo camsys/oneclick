469809cdfb61051ff61d4f7ab19eae47024b731f
Derek Edwards <dedwards8@gmail.com>
2016-09-25 13:04:08 -0400

    Update assets rules for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,19 +1,19 @@
 Oneclick::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.cache_classes = true
+  #config.cache_classes = true
 
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # config.action_dispatch.best_standards_support
 
-  config.serve_static_assets = true
+  #config.serve_static_assets = true
 
-  config.assets.compress = true
-  config.assets.compile = false
-  config.assets.digest = true
-  # config.assets.debug
+  #config.assets.compress = true
+  #config.assets.compile = false
+  #config.assets.digest = true
+  #config.assets.debug
 
   config.i18n.fallbacks = false
 
