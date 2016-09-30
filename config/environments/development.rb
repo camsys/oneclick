e910dace653e8ffa4d4685b16de49a61b5a056b5
soynog <nicolas.o.garcia@gmail.com>
2016-09-30 10:06:16 -0400

    Update Dev Environment to properly display asset paths in email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,7 @@ Oneclick::Application.configure do
 
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  config.action_controller.asset_host = 'http://localhost:3000' # So assets point to correct location
 
   config.action_dispatch.best_standards_support = :builtin
 
@@ -31,6 +32,7 @@ Oneclick::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.default_url_options = { :host => 'localhost:3000' } #use localhost when in development
+  config.action_mailer.asset_host = config.action_controller.asset_host # So that email images will show up
 
   config.log_level = :debug
   config.logger = Logger.new(STDOUT)
