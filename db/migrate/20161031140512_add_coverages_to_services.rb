95b0a64e69c5e79ff0ca9f631242381bda1c6408
soynog <nicolas.o.garcia@gmail.com>
2016-10-31 10:32:00 -0400

    Remove add_foreign_key calls

--- db/migrate/20161031140512_add_coverages_to_services.rb
+++ db/migrate/20161031140512_add_coverages_to_services.rb
@@ -1,8 +1,6 @@
 class AddCoveragesToServices < ActiveRecord::Migration
   def change
     add_reference :services, :primary_coverage, references: :coverages, index: true
-    add_foreign_key :services, :coverages, column: :primary_coverage_id
     add_reference :services, :secondary_coverage, references: :coverages, index: true
-    add_foreign_key :services, :coverages, column: :secondary_coverage_id
   end
 end
