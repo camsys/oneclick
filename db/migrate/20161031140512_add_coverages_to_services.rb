b4dcdf5afc6fdd52515288446ec3d14dd3cc093d
soynog <nicolas.o.garcia@gmail.com>
2016-10-31 10:43:00 -0400

    Rename Coverages to CoverageZones

--- db/migrate/20161031140512_add_coverages_to_services.rb
+++
@@ -1,6 +0,0 @@
-class AddCoveragesToServices < ActiveRecord::Migration
-  def change
-    add_reference :services, :primary_coverage, references: :coverages, index: true
-    add_reference :services, :secondary_coverage, references: :coverages, index: true
-  end
-end
