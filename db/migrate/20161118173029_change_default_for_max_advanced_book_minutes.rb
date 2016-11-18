89d968b2da2d749a765eb3813d5dc909c5bb45f1
soynog <nicolas.o.garcia@gmail.com>
2016-11-18 12:48:24 -0500

    Fix and Run Migration to add Default Advance Book Minutes

--- db/migrate/20161118173029_change_default_for_max_advanced_book_minutes.rb
+++ db/migrate/20161118173029_change_default_for_max_advanced_book_minutes.rb
@@ -1,9 +1,9 @@
 class ChangeDefaultForMaxAdvancedBookMinutes < ActiveRecord::Migration
   def self.up
-    change_column :services, :max_advanced_book_minutes, default: 525600
+    change_column :services, :max_advanced_book_minutes, :integer, :default => 525600
   end
 
   def self.down
-    change_column :services, :max_advanced_book_minutes, default: 0
+    change_column :services, :max_advanced_book_minutes, :integer, :default => 0
   end
 end
