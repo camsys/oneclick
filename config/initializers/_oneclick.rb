5605052e25befd11e1125b2b9ed113c9a8ea9362
Nicolas Garcia <ngarcia@camsys.com>
2016-11-18 10:35:42 -0500

    New Logo File to Force Refresh

--- config/initializers/_oneclick.rb
+++ config/initializers/_oneclick.rb
@@ -104,7 +104,7 @@ Oneclick::Application.configure do
 
   config.host = 'oneclick.camsys-apps.com'
   config.logo_text = "1-Click"
-  config.ui_logo = 'logo.png'
+  config.ui_logo = 'logo_new.png'
   config.logo_text = "1-Click"
   config.favicon = ''
   config.mobile_favicon = ''
