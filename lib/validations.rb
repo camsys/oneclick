5ce4c5f3ce83deea130e23d71f60c1cf527042b1
Derek Edwards <dedwards8@gmail.com>
2016-11-18 23:25:54 -0500

    Don't put an http:// on an empty service url.

--- lib/validations.rb
+++ lib/validations.rb
@@ -1,7 +1,7 @@
 module Validations
 
   def check_url_protocol
-    unless self.url.nil?
+    unless self.url.blank?
       unless self.url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//]
         self.url = "http://#{self.url}"
       end
