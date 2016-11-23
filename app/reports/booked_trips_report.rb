4ce151d8ded7fa57261079c4aa1c865fb4612c98
soynog <nicolas.o.garcia@gmail.com>
2016-11-23 13:20:32 -0500

    Continue Building Booked Trips Report
    All (mostly) working:
    - All trips by time unit
    - Booked trips by time unit
    - Trip purposes

--- app/reports/booked_trips_report.rb
+++ app/reports/booked_trips_report.rb
@@ -28,108 +28,103 @@ class BookedTripsReport < AbstractReport
     @to_date = Chronic.parse(report.to_date).to_date
     @date_range = @from_date..@to_date
     @time_unit = UNITS_OF_TIME[report.booked_trips_date_option.to_sym]
-    itinerary_base = Itinerary.valid.visible # Base query -- all valid itineraries
+    itinerary_base = Itinerary.valid.visible.where(created_at: @date_range) # Base query -- all valid itineraries
     data = {} # Object for holding results tables
 
-    # Populate All Booked Trip Counts Data Table
+    ##########################
+    # All Booked Trips Count #
+    ##########################
+
+    booked_itins = itinerary_base.where.not(booking_confirmation: nil)
+
+    # Prepare Data Table
     data[:all_booked_trips] = {
-      table: [[@time_unit.to_s, "trips booked"]] +
-        itinerary_base.parcel_by(@date_range, @time_unit) { |seg, data| [seg.send(@time_unit).to_s, data.where.not(booking_confirmation: nil).count]},
+      table: [[@time_unit.to_s, "trips booked"]],
       visualization: 'ColumnChart',
       options: {
-        title: "Trips Booked by #{@time_unit.to_s.titleize}",
-        width: 600,
-        height: 400
+        title: "Total Trips Booked, by #{@time_unit.to_s.titleize}",
+        width: 1000,
+        height: 600
       }
     }
 
-    # (earliest_itin_date..Date.today).select { |d| d.day == 1 }.each do |m|
-    #   data[:booked_trips_by_month][:table] << [m.year.to_s, itinerary_base.where(created_at: (Date.new(m.year,m.month,1)..Date.new(m.year,m.month,-1))).where.not(booking_confirmation: nil).count ]
-    # end
+    # Add Data to the Table
+    data[:all_booked_trips][:table] +=
+    booked_itins.parcel_by(@date_range, @time_unit) do |seg, data|
+      [seg.send(@time_unit).to_s, data.count]
+    end
 
+    ##########################
+    # Trips Selected by Mode #
+    ##########################
+
+    selected_itins = itinerary_base.selected
+
+    # Prepare Data Table
+    data[:selected_trips_by_mode] = {
+      table: [],
+      visualization: 'ColumnChart',
+      options: {
+        title: "Mode of Trips Selected, by #{@time_unit.to_s.titleize}",
+        width: 1000,
+        height: 600,
+        isStacked: true
+      }
+    }
+
+    # Create Column Headers
+    data[:selected_trips_by_mode][:table] << ([@time_unit.to_s] + Mode.all.map {|m| TranslationEngine.translate_text(m.name)})
+
+    # Add Data to the Table
+    data[:selected_trips_by_mode][:table] +=
+    selected_itins.parcel_by(@date_range, @time_unit) do |seg, data|
+      [seg.send(@time_unit).to_s] + Mode.all.map {|m| data.where(mode_id: m.id).count }
+    end
+
+    ###########################
+    # Trips Booked by Purpose #
+    ###########################
+
+    # Query all itineraries with and without a trip_purpose_raw value (for external booking services)
+    itins_with_purpose = itinerary_base.includes(trip_part: [trip: [:trip_purpose]])
+    itins_with_external_purpose = itins_with_purpose.where.not(trips: {trip_purpose_raw: nil})
+    itins_without_external_purpose = itins_with_purpose.where(trips: {trip_purpose_raw: nil})
+
+    # Prepare Data Table
+    data[:trip_purposes] = {
+      table: [],
+      visualization: 'PieChart',
+      options: {
+        title: "Mode of Trips Selected, by #{@time_unit.to_s.titleize}",
+        width: 1000,
+        height: 600
+      }
+    }
+
+    # Create Column Headers
+    data[:trip_purposes][:table] << ['purpose', 'trip count']
+
+    # Make a list of trip purposes
+    purpose_list = (
+      TripPurpose.all.map {|tp| tp.code} +
+      Trip.where.not(trip_purpose_raw: nil).map {|t| t.trip_purpose_raw.parameterize.underscore}
+    ).uniq
+
+    # Add Data to the Table
+    purpose_list.each do |tp|
+      data[:trip_purposes][:table] << [tp,
+        itins_with_external_purpose.where(:trips => {trip_purpose_raw: tp.humanize.titleize}).count +
+        itins_without_external_purpose.where(:trip_purposes => {:code => tp}).count
+      ] # REFACTOR trip_purpose_raw -- doesn't work for all trip_purposes
+    end
+
+
+    ###########################
+
+    # Return Data Hash
     puts "DATA IS: ", data.ai
     data
 
-    # # Set the date range using a DateOption object
-    # date_option = DateOption.find(report.date_range)
-    # date_option ||= DateOption.find_by(code: DateOption::DEFAULT)
-    # date_range = date_option.get_date_range(report.from_date, report.to_date)
-    #
-    # # Make an empty hash to hold the results data
-    # data = Hash.new
-    #
-    # # Zip together class names and column names into an array of arrays and iterate through each element
-    # @totals_class_names.zip(@totals_cols).each do |name, col|
-    #   # If class has a created_at method, count objects of that class that were created within the date range.
-    #   # ACTUALLY, this doesn't seem to work. Everything returns false for method_defined? :created_at
-    #   if name.constantize.method_defined? :created_at
-    #     data[col] = name.constantize.where(created_at: date_range).count
-    #   else # otherwise, just count all of them.
-    #     data[col] = name.constantize.count
-    #   end
-    # end
-    #
-    # # Put together User data
-    # @user_cols.each do |col|
-    #   case col
-    #   when :total_users
-    #     data[col] = User.where(created_at: date_range).count
-    #   when :active_users
-    #     data[col] = User.where(last_sign_in_at: date_range).count
-    #   when :total_logins_by_active_users
-    #     data[col] = User.where(last_sign_in_at: date_range).sum(:sign_in_count)
-    #   when :totals_by_locale
-    #     data[col] = User.where(created_at: date_range).group(:preferred_locale).count(:preferred_locale)
-    #   end
-    # end
-    #
-    # trip_base = Trip.where(scheduled_time: date_range)
-    # trip_base = trip_base.where(agency_id: report.agency_id) if report.agency_id
-    # trip_base = trip_base.where(creator_id: report.agent_id).where.not(user_id: report.agent_id) if report.agent_id
-    # trip_base = trip_base.where("outbound_provider_id = ? OR return_provider_id = ?",
-    #                             report.provider_id, report.provider_id) if report.provider_id
-    #
-    # itinerary_base = Itinerary.valid.visible.where(start_time: date_range)
-    # itinerary_base = itinerary_base.joins(:service)
-    #   .where(services: {provider_id: report.provider_id}) if report.provider_id
-    #
-    # # Put together trips data
-    # @trip_cols.each do |col|
-    #   # Create a key in the data hash for each column, and set its value based on the column name.
-    #   data[col] = case col
-    #   # Trips
-    #   when :total_trips
-    #     trip_base.count
-    #   when :trips_by_ui_mode
-    #     trip_base.where.not(ui_mode: nil).group(:ui_mode).count(:ui_mode)
-    #   # Itineraries
-    #   when :total_itineraries_generated
-    #     itinerary_base.count
-    #   when :total_itineraries_selected
-    #     itinerary_base.where(selected: true).count
-    #   when :bookings
-    #     itinerary_base.where.not(booking_confirmation: nil).count
-    #   when :generated_itineraries_by_mode
-    #     itinerary_base.group(:mode_id).count
-    #   when :selected_itineraries_by_mode
-    #     itinerary_base.where(selected: true).group(:mode_id).count
-    #   end
-    # end
-    #
-    # @rating_cols.each do |col|
-    #   data[col] = case col
-    #   when :total_ratings
-    #     Rating.where(status: "approved", updated_at: date_range).count
-    #   when :average_rating
-    #     Rating.where(status: "approved", updated_at: date_range).average(:value)
-    #   end
-    # end
-    #
-    # data
-  end
-
-  def get_columns
-    @totals_cols + @user_cols + @trip_cols
   end
 
   def self.available_date_option_collections
