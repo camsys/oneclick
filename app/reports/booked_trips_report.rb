7aff6e9fa77ca1f47d5b54b9975f203c1659a560
soynog <nicolas.o.garcia@gmail.com>
2016-11-25 15:25:31 -0500

    Update Booked Trips Report View to use JS Date objects

--- app/reports/booked_trips_report.rb
+++ app/reports/booked_trips_report.rb
@@ -29,29 +29,35 @@ class BookedTripsReport < AbstractReport
     @date_range = @from_date..@to_date
     @time_unit = UNITS_OF_TIME[report.booked_trips_date_option.to_sym]
     itinerary_base = Itinerary.valid.visible.where(created_at: @date_range) # Base query -- all valid itineraries
+    booked_itins = itinerary_base.where.not(booking_confirmation: nil)
     data = {} # Object for holding results tables
 
     ##########################
     # All Booked Trips Count #
     ##########################
 
-    booked_itins = itinerary_base.where.not(booking_confirmation: nil)
-
     # Prepare Data Table
     data[:all_booked_trips] = {
-      table: [[@time_unit.to_s, "trips booked"]],
+      columns: [],
+      rows: [],
       visualization: 'ColumnChart',
       options: {
         title: "Total Trips Booked, by #{@time_unit.to_s.titleize}",
         width: 1000,
-        height: 600
+        height: 600,
+        hAxis: {}
       }
     }
 
+    # Add columns
+    data[:all_booked_trips][:columns] = [
+      { heading: @time_unit.to_s, type: 'date'},
+      { heading: "trips booked", type: 'number'}
+    ]
+
     # Add Data to the Table
-    data[:all_booked_trips][:table] +=
-    booked_itins.parcel_by(@date_range, @time_unit) do |seg, data|
-      [seg.send(@time_unit).to_s, data.count]
+    data[:all_booked_trips][:rows] = booked_itins.parcel_by(@date_range, @time_unit) do |seg, data|
+      [seg, data.count]
     end
 
     ##########################
@@ -62,7 +68,8 @@ class BookedTripsReport < AbstractReport
 
     # Prepare Data Table
     data[:selected_trips_by_mode] = {
-      table: [],
+      columns: [],
+      rows: [],
       visualization: 'ColumnChart',
       options: {
         title: "Mode of Trips Selected, by #{@time_unit.to_s.titleize}",
@@ -73,12 +80,15 @@ class BookedTripsReport < AbstractReport
     }
 
     # Create Column Headers
-    data[:selected_trips_by_mode][:table] << ([@time_unit.to_s] + Mode.all.map {|m| TranslationEngine.translate_text(m.name)})
+    data[:selected_trips_by_mode][:columns] = [{heading: @time_unit.to_s, type: 'date'}]
+    data[:selected_trips_by_mode][:columns] += Mode.all.map do |m|
+      { heading: TranslationEngine.translate_text(m.name), type: 'number' }
+    end
 
     # Add Data to the Table
-    data[:selected_trips_by_mode][:table] +=
+    data[:selected_trips_by_mode][:rows] =
     selected_itins.parcel_by(@date_range, @time_unit) do |seg, data|
-      [seg.send(@time_unit).to_s] + Mode.all.map {|m| data.where(mode_id: m.id).count }
+      [seg] + Mode.all.map {|m| data.where(mode_id: m.id).count }
     end
 
     ###########################
@@ -87,44 +97,50 @@ class BookedTripsReport < AbstractReport
     # NOTE: Currently only set up to use trip_purpos_raw field from external booking services
 
     # Query all itineraries with and without a trip_purpose_raw value (for external booking services)
-    itins_with_purpose = itinerary_base.includes(trip_part: [trip: [:trip_purpose]])
+    itins_with_purpose = booked_itins.includes(trip_part: [trip: [:trip_purpose]])
     itins_with_external_purpose = itins_with_purpose.where.not(trips: {trip_purpose_raw: nil})
     itins_without_external_purpose = itins_with_purpose.where(trips: {trip_purpose_raw: nil})
 
     # Prepare Data Table
     data[:trip_purposes] = {
-      table: [],
+      columns: [],
+      rows: [],
       visualization: 'PieChart',
       options: {
-        title: "Mode of Trips Selected",
+        title: "Purpose of Trips Selected",
         width: 1000,
         height: 600
       }
     }
 
     # Create Column Headers
-    data[:trip_purposes][:table] << ['purpose', 'trip count']
+    data[:trip_purposes][:columns] = [
+      { heading: 'purpose', type: 'string'},
+      { heading: 'trip count', type: 'number'}
+    ]
 
-    # Make a list of trip purposes
-    purpose_list = (
-      Trip.where.not(trip_purpose_raw: nil).map {|t| t.trip_purpose_raw}
-    ).uniq
+    # Group and count itineraries by (external) trip purpose
+    purpose_counts = itins_with_external_purpose.group("trips.trip_purpose_raw").count
+
+    # Lump all but the top X purposes into an other category.
+    top_x = [10, purpose_counts.keys.length].min
+    other_purposes = purpose_counts.select {|k,v| v < purpose_counts.values.sort[-top_x] }
+    purpose_counts["All Other Purposes"] = other_purposes.values.sum
+    other_purposes.each {|k,v| purpose_counts.delete(k)}
 
     # Add Data to the Table
-    purpose_list.each do |tp|
-      data[:trip_purposes][:table] << [tp,
-        itins_with_external_purpose.where(:trips => {trip_purpose_raw: tp}).count
-      ]
-    end
+    purpose_counts.each { |purpose, count| data[:trip_purposes][:rows] << [purpose, count] }
 
     ##########################
     # Status of Booked Trips #
     ##########################
 
-    itins_with_ecolane_booking = itinerary_base.includes(:ecolane_booking).where.not(ecolane_bookings: {id: nil}).references(:ecolane_bookings)
+    itins_with_ecolane_booking = booked_itins.includes(:ecolane_booking).where.not(ecolane_bookings: {id: nil}).references(:ecolane_bookings)
 
     # Prepare Data Table
     data[:booked_trip_status] = {
+      columns: [],
+      rows: [],
       table: [],
       visualization: 'PieChart',
       options: {
@@ -135,16 +151,19 @@ class BookedTripsReport < AbstractReport
     }
 
     # Create Column Headers
-    data[:booked_trip_status][:table] << ['status', 'trip count']
+    data[:booked_trip_status][:columns] = [
+      { heading: 'status', type: 'string'},
+      { heading: 'trip count', type: 'number'}
+    ]
 
     status_counts = itins_with_ecolane_booking.group('ecolane_bookings.booking_status_code').count
 
     # Add Data to the Table
     status_counts.each do |status, count|
-      data[:booked_trip_status][:table] << [status.strip, count] if status
+      data[:booked_trip_status][:rows] << [status.strip, count] if status
     end
 
-    ###########################
+    ##########################
 
     # Return Data Hash
     puts "DATA IS: ", data.ai
