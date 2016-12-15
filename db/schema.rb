449dc4ab4b43f17002a0a6a4cef2372974269d64
Derek Edwards <dedwards8@gmail.com>
2016-12-15 17:37:30 -0500

    Add foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214201941) do
+ActiveRecord::Schema.define(version: 20161215222357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -970,4 +970,7 @@ ActiveRecord::Schema.define(version: 20161214201941) do
     t.datetime "updated_at"
   end
 
+  add_foreign_key "itineraries", "trip_parts"
+  add_foreign_key "trip_parts", "trips"
+  add_foreign_key "trip_places", "trips"
 end
