d75175ebbe2348493eaa2f4db0d7b18cbedd628f
Derek Edwards <dedwards8@gmail.com>
2016-12-15 21:29:23 -0500

    Update trips_desired_modes FK

--- db/migrate/20161216021602_addmoreforeignkeys.rb
+++ db/migrate/20161216021602_addmoreforeignkeys.rb
@@ -1,6 +1,6 @@
 class Addmoreforeignkeys < ActiveRecord::Migration
   def change
     add_foreign_key :trips_desired_modes, :trips
-    add_foreign_key :trips_desired_modes, :desired_modes
+    add_foreign_key :trips_desired_modes, :modes, column: :desired_mode_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215222357) do
+ActiveRecord::Schema.define(version: 20161216021602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -973,4 +973,6 @@ ActiveRecord::Schema.define(version: 20161215222357) do
   add_foreign_key "itineraries", "trip_parts"
   add_foreign_key "trip_parts", "trips"
   add_foreign_key "trip_places", "trips"
+  add_foreign_key "trips_desired_modes", "modes", column: "desired_mode_id"
+  add_foreign_key "trips_desired_modes", "trips"
 end
