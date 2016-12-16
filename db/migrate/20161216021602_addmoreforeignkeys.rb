42ed59ad68a3923a7e3996825b8b8d35ee761d66
Derek Edwards <dedwards8@gmail.com>
2016-12-15 21:41:21 -0500

    Delete migration

--- db/migrate/20161216021602_addmoreforeignkeys.rb
+++
@@ -1,6 +0,0 @@
-class Addmoreforeignkeys < ActiveRecord::Migration
-  def change
-    add_foreign_key :trips_desired_modes, :trips
-    add_foreign_key :trips_desired_modes, :modes, column: :desired_mode_id
-  end
-end
