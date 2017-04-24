198cf5ad8bb38b7ef20a388b5e3346a2e2a338be
soynog <nicolas.o.garcia@gmail.com>
2017-04-24 15:06:01 -0400

    Log stack trace when catching api errors

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -30,6 +30,7 @@ module Api
 
       # Rescues 500 errors and renders them properly as JSON response
       def api_error_response(exception)
+        exception.backtrace.each { |line| logger.error line }
         response = {
           error: { type: exception.class.name, message: exception.message }
         }
