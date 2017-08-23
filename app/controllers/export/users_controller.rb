2b073fbb1e0d8c81eddb6c0551c7feb726dbdd54
Derek Edwards <dedwards8@gmail.com>
2017-08-23 16:16:12 -0400

    Batch users

--- app/controllers/export/users_controller.rb
+++ app/controllers/export/users_controller.rb
@@ -6,8 +6,12 @@ module Export
     end
     
     def guests
+
+      batch_index = params[:batch_index].try(:to_i) || 0
+      batch_size = params[:batch_size].try(:to_i) || 50
+
       @professinals = get_professionals
-      @users = (User.where.not(id: User.registered.pluck(:id)).joins(:trips).uniq) - @professinals
+      @users = (User.where.not(id: User.registered.pluck(:id)).order(:id).limit(batch_size).offset(batch_size*batch_index).joins(:trips).uniq) - @professinals
       render json: @users.map{ |u| UserSerializer.new(u).serializable_hash }
     end
     
